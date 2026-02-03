bb4a40a5fc90775c957074581dcd6dafe7e85df9
erzan <earl.do@avegabros.com>
2026-02-03 15:25:59 +0800

    added api properties to dto and controllers
    for user readability

--- src/modules/administrator/module/dto/create-module.dto.ts
+++ src/modules/administrator/module/dto/create-module.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
+ApiProperty
 
 export class CreateModuleDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Operations',
+    description: 'The name of the module'
+  })
   name: string;
 }
--- src/modules/administrator/module/dto/update-module.dto.ts
+++ src/modules/administrator/module/dto/update-module.dto.ts
@@ -4,9 +4,17 @@ import { IsString, IsNotEmpty, IsOptional, IsInt } from 'class-validator';
 export class UpdateModuleDto {
   @IsNotEmpty()
   @IsInt()
+  @ApiProperty({
+    example: 1,
+    description: 'The id of the module to update'
+  })
   module_id: number;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    example: 'New module name',
+    description: 'Update current module name'
+  })
   name: string;
 }
