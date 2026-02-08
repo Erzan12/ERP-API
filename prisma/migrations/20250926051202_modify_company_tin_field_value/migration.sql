4eba083d8998ebfc61f28e9f7d60fce52f0236d2
erzan <do.earljan@gmail.com>
2026-02-08 18:17:08 +0800

    remove old migrations and created a new one using uuid

--- prisma/migrations/20250926051202_modify_company_tin_field_value/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropIndex
-DROP INDEX "public"."Company_company_tin_key";
-
--- AlterTable
-ALTER TABLE "public"."Company" ALTER COLUMN "abbreviation" SET DATA TYPE VARCHAR(100),
-ALTER COLUMN "company_tin" SET DATA TYPE TEXT;
