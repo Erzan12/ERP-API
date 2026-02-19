ee6309e7903b0d2758ba23521f017a4931f6417e
erzan <earl.do@avegabros.com>
2026-02-19 16:13:57 +0800

    added swagger api operation in db query submoudle in administrator

--- src/modules/administrator/db-query/db-query.controller.ts
+++ src/modules/administrator/db-query/db-query.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Post, Body, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ACTION_CREATE, ACTION_READ, SYSTEM_MANAGEMENT } from 'src/utils/constants/ability.constant';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags, ApiBody } from '@nestjs/swagger';
 import { DbQueryService } from './db-query.service';
 import { Can } from 'src/utils/decorators/can.decorator';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 import { ExecuteDbQueryDto } from './dto/execute-db-query.dto';
+import { ApiPostResponse, ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
 
 @ApiBearerAuth('access-token')
 @ApiTags('Administrator - Database Manuel Query')
@@ -14,7 +15,9 @@ export class DbQueryControllerV2 {
     constructor(private readonly dbQueryService: DbQueryService) {}
 
     @Post('execute')
+    @ApiBody({ type: ExecuteDbQueryDto, description: 'Payload for manual db query'})
     @ApiOperation({ summary: 'Execute manual SQL query (Super Admin Only)' })
+    @ApiPostResponse('Db manual query successful')
     @Can({ action: ACTION_CREATE, subject: SYSTEM_MANAGEMENT })
     executeQuery(
         @Body() dto: ExecuteDbQueryDto,
@@ -25,6 +28,7 @@ export class DbQueryControllerV2 {
 
     @Get('logs')
     @ApiOperation({ summary: 'Get latest executed manual queries' })
+    @ApiGetResponse('Logs for all manual db queries performed')
     @Can({ action: ACTION_READ, subject: SYSTEM_MANAGEMENT })
     getLogs() {
         return this.dbQueryService.getLogs();
@@ -32,6 +36,7 @@ export class DbQueryControllerV2 {
 
     @Get('logs/:id')
     @ApiOperation({ summary: 'Get specific manual query log'})
+    @ApiGetResponse('Get a specific manual db query log')
     @Can({ action: ACTION_READ, subject: SYSTEM_MANAGEMENT })
     getLog(@Param('id', new ParseUUIDPipe()) id: string) {
         return this.dbQueryService.getLogById(id);
