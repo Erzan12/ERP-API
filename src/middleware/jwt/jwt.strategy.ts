7fcf5675ca5f1488c43ac6da2eb430632f498bcf
erzan <earl.do@avegabros.com>
2026-02-25 13:22:50 +0800

    added new column in user table adjusted auth controller and service
    added authenticated decorator and api bearer auth in auth controller log out api, adjusted jwt payload added department_id and tokenVersion, in get user method adjusted department selecting id and name columns, in jwt validator added check if token is still valid if user log out

--- src/middleware/jwt/jwt.strategy.ts
+++ src/middleware/jwt/jwt.strategy.ts
@@ -4,122 +4,6 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor(private prisma: PrismaService) {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: process.env.JWT_SECRET || 'secret-key',
-//       passReqToCallback: true,
-//     });
-//   }
-
-//   async validate(req: Request, payload: any) {
-
-//     console.log('Correct payload:', payload);         // Should now show { sub: 3, ... }
-//     console.log('payload.sub:', payload.sub);         // Should now show 3
-
-//     // const user = await this.prisma.user.findUnique({
-//     //   where: { id: payload.sub },
-//     //   include: {
-//     //     user_roles: {
-//     //       include: {
-//     //       //   role: {
-//     //           // include: {
-//     //             role_permission: {
-//     //               include: {
-//     //                 // permission: true,
-//     //                 sub_module_permission: true
-//     //               },
-//     //             },
-//     //         //   },
-//     //         // },
-//     //         module: true,
-//     //       },
-//     //     },
-//     //     // module: true,
-//     //   },
-//     // });
-//     const user = await this.prisma.user.findUnique({
-//       where: { id: payload.sub },
-//       include: {
-//         user_roles: {
-//           include: {
-//             role_permission: {
-//               include: {
-//                 sub_module_permission: {
-//                   include: {
-//                     added_sub_mod_permission: true,
-//                   },
-//                 },
-//               },
-//             },
-//             // module: true,
-//           },
-//         },
-//       },
-//     });
-
-//     if (!user || !user.is_active || !user.user_roles) {
-//       throw new UnauthorizedException('Invalid or inactive user');
-//     }
-//  //returns only the necessary user details needed for auth and role and permission
-//     // return {
-//     //   id: user.id,
-//     //   email: user.email,
-//     //   roles: user.user_roles.map((ur) => ({
-//     //     //replaced with direct role permission
-//     //     // id: ur.role_id,
-//     //     // name: ur.role.name,
-//     //     //role permission is combined role and the permission for that role with submodule
-//     //     id: ur.role_permission_id,
-//     //     name: ur.role_permission.role_name,
-//     //     //handle multi module per user
-//     //     module: {
-//     //       id: ur.module.id,
-//     //       name: ur.module.name,
-//     //     },
-//     //   // permission: ur.role_permission.map((rp) => ({
-//     //   //     action: rp.action,
-//     //   //     permission: { name: rp.sub_module_permission_id.id },
-//     //   //     // status: rp.status,
-//     //   //   })),
-//     //   // })),
-//     //   // user has only one role_permission per user_role
-//     //   permission: [{
-//     //       action: ur.role_permission.action,
-//     //       permission: {
-//     //         name: ur.role_permission.sub_module_permission?.added_sub_mod_permission?.action,
-//     //       },
-//     //     }],
-//     //   })),
-//     //   // user have multiple role permissions
-//     //   // permission: ur.role_permissions.map((rp) => ({
-//     //   //   action: rp.action,
-//     //   //   permission: {
-//     //   //     name: rp.sub_module_permission?.added_sub_mod_permission?.action,
-//     //   //   },
-//     //   // })),
-//     // };
-//     //revamped method of return without module
-//     return {
-//       id: user.id,
-//       email: user.email,
-//       roles: user.user_roles.map((ur) => ({
-//         id: ur.role_permission_id,
-//         name: ur.role_permission.role_name,
-//         permissions: [{
-//           action: ur.role_permission.action,
-//           permission: {
-//             name: ur.role_permission.sub_module_permission?.added_sub_mod_permission?.action,
-//           },
-//         }],
-//       })),
-//     };
-//   }
-// }
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private prisma: PrismaService) {
@@ -142,7 +26,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         user_roles: {
           include: {
             role: true,
-
             user_permissions: {
               include: {
                 role_permission: {
@@ -163,13 +46,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException('User not found or invalid token');
     }
 
+    if (payload.tokenVersion !== user.token_version) {
+      throw new UnauthorizedException('Token has been invalidated');
+    }
+
     const employee = user.employee;
 
     return {
       id: user.id,
       email: user.email,
       // username: user.username,
-      department_id: employee.employee_id,
+      department_id: employee.department_id,
       security_clearance_level: user.security_clearance_level ?? 0,
       roles: user.user_roles.map((ur) => ({
         id: ur.role?.id ?? 0,
