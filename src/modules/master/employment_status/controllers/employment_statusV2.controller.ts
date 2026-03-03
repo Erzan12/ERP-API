4756f7859a05b5ae2c59cea0b70e12e5753dbf33
erzan <earl.do@avegabros.com>
2026-03-03 14:11:09 +0800

    employment status pagination done

--- src/modules/master/employment_status/controllers/employment_statusV2.controller.ts
+++ src/modules/master/employment_status/controllers/employment_statusV2.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   ParseIntPipe,
   ParseUUIDPipe,
+  Query,
 } from '@nestjs/common';
 import { EmploymentStatusService } from '../employment_status.service';
 import { Can } from '../../../../utils/decorators/can.decorator';
@@ -25,6 +26,7 @@ import {
   ACTION_UPDATE,
   MASTERTABLES,
 } from 'src/utils/constants/ability.constant';
+import { PaginationDto } from 'src/utils/dtos/pagination.dto';
 
 @ApiTags('Mastertable - Employment Status')
 @Controller({ path: 'mastertable', version: '2' })
@@ -32,12 +34,20 @@ export class EmploymentStatusControllerV2 {
   constructor(private employmentStatusService: EmploymentStatusService) {}
 
   //get all employment_status
-  @Get('employment_status/')
+  @Get('employment_status')
   @ApiOperation({ summary: 'Get all employment status' })
   @ApiGetResponse('Here are the list of available employment status')
   @Can({ action: ACTION_READ, subject: MASTERTABLES })
-  getEmployeeStats(@SessionUser() user: RequestUser) {
-    return this.employmentStatusService.getEmployeeStats(user);
+  getEmployeeStats(
+    @SessionUser() user: RequestUser,
+    @Query() dto: PaginationDto,
+    @Query('page') page = 1,
+    @Query('perPage') perPage = 10,
+    @Query('search') search?: string,
+    @Query('sortBy') sortBy: string = 'created_at',
+    @Query('order') order: 'asc' | 'desc' = 'asc',
+  ) {
+    return this.employmentStatusService.getEmployeeStats(user, dto);
   }
 
   //get only one employment_status
