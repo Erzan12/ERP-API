512da53ffb5750f7dc6c09e7f3746a577be7261d
erzan <earl.do@avegabros.com>
2026-03-03 10:19:01 +0800

    added role management submodule in hris
    added controller and service for role management, added new ability const for role management to use for perm check

--- src/modules/hris/hrV1.module.ts
+++ src/modules/hris/hrV1.module.ts
@@ -5,11 +5,13 @@ import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardControllerV1 } from './dashboard/controllers/dashboardV1.controller';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { RoleManagementControllerV2 } from './role-management/controllers/role-managementV2.controller';
+import { RoleManagementService } from './role-management/role-management.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [EmployeeService, PrismaService, DashboardService],
-  controllers: [EmployeeControllerV1, DashboardControllerV1],
+  providers: [EmployeeService, PrismaService, DashboardService, RoleManagementService],
+  controllers: [EmployeeControllerV1, DashboardControllerV1, RoleManagementControllerV2],
   exports: [HrV1Module],
 })
 export class HrV1Module {}
