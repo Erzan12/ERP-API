10d758f7af2c8571d5c44d52e3fe3a8f427a3067
erzan <earl.do@avegabros.com>
2026-03-06 13:17:06 +0800

    refactored mastertable position service and dto
    updated position dto to use uuid instead of int, added new columns in position job description and hierarchy, new migratioin files added created by and updated by

--- src/modules/master/position/dto/position.dto.ts
+++ src/modules/master/position/dto/position.dto.ts
@@ -1,5 +1,5 @@
 import { Expose, Transform } from 'class-transformer';
-import { IsDefined, IsNotEmpty, IsString, IsInt, IsOptional } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsString, IsInt, IsOptional, IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { BadRequestException } from '@nestjs/common';
 
@@ -12,29 +12,46 @@ export class CreatePositionDto {
   })
   name: string;
 
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    example: 'Supervisor',
+    description: 'The hierarchy of this position',
+  })
+  hierarchy?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    example: 'Supervises the development and life cycle of systems',
+    description: 'The job description fit for this position',
+  })
+  job_description?: string;
+
+  @IsOptional()
   @IsInt()
   @ApiProperty({ example: 2, description: 'Sorting number of the position' })
   sorting?: number;
 
-  @IsInt()
+  @IsUUID()
   @IsDefined()
   @Expose({ name: 'department' })
   @ApiProperty({
     name: 'department',
-    example: 'human resources = 1, information technology = 2, accounting = 6',
+    example: 'Department PK UUID',
     description: 'The Department where the position is available',
   })
-  @Transform(({ value }) => {
-    console.log('Transforming status:', value);
-    if (value === 'human resources')
-      return 'ddc33ca7-6088-4368-9c95-86f4b30e3cfd';
-    if (value === 'information technology')
-      return '4c0647c8-2b00-49dd-8a82-deee685dd95d';
-    if (value === 'accounting') return '0fd6a4fb-6e39-4b98-9d8e-de76fa52e0f9';
-    throw new BadRequestException(
-      `Invalid status value ${value}. Allowed values are "active" or "inactive"`,
-    );
-  })
+  // @Transform(({ value }) => {
+  //   console.log('Transforming status:', value);
+  //   if (value === 'human resources')
+  //     return 'ddc33ca7-6088-4368-9c95-86f4b30e3cfd';
+  //   if (value === 'information technology')
+  //     return '4c0647c8-2b00-49dd-8a82-deee685dd95d';
+  //   if (value === 'accounting') return '0fd6a4fb-6e39-4b98-9d8e-de76fa52e0f9';
+  //   throw new BadRequestException(
+  //     `Invalid status value ${value}. Allowed values are "active" or "inactive"`,
+  //   );
+  // })
   department_id: string;
 
   // @IsInt()
