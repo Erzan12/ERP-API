e84f6940f29b89a9d8dc5e9492ba7e6d50a87cc4
erzan <earl.do@avegabros.com>
2026-03-12 13:17:11 +0800

    scaffolding for career posting hris

---
+++ src/modules/hris/career-posting/controllers/career-posting-v2.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('career-posting-v2')
+export class CareerPostingV2Controller {}
--- src/modules/hris/hrV1.module.ts
+++ src/modules/hris/hrV1.module.ts
@@ -5,11 +5,14 @@ import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardControllerV1 } from './dashboard/controllers/dashboardV1.controller';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { CareerPostingController } from './career_posting/career_posting.controller';
+import { CareerPostingV2Controller } from './career_posting-v2/career_posting-v2.controller';
+import { CareerPostingService } from './career-posting/career-posting.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [EmployeeService, PrismaService, DashboardService],
-  controllers: [EmployeeControllerV1, DashboardControllerV1],
+  providers: [EmployeeService, PrismaService, DashboardService, CareerPostingService],
+  controllers: [EmployeeControllerV1, DashboardControllerV1, CareerPostingController, CareerPostingV2Controller],
   exports: [HrV1Module],
 })
 export class HrV1Module {}
