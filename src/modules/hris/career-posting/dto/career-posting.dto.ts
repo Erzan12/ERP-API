9e85e797e26b1cd4d0a826035d1f0c292c0a3cdd
erzan <earl.do@avegabros.com>
2026-03-14 09:42:47 +0800

    added career posting get and update api

--- src/modules/hris/career-posting/dto/career-posting.dto.ts
+++ src/modules/hris/career-posting/dto/career-posting.dto.ts
@@ -1,7 +1,9 @@
 import { 
+    IsBoolean,
     IsEnum, 
     IsInt, 
     IsNotEmpty, 
+    IsOptional, 
     IsString, 
     IsUUID
 } from "class-validator";
@@ -72,4 +74,89 @@ export class CreateCareerPostingDto {
         description: 'The PK uuid of the user location',
     })
     user_location_id: string;
+}
+
+export class UpdateCareerPostingDto {
+    @IsOptional()
+    @IsUUID()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'Position PK UUID',
+        description: 'The PK uuid of the position',
+    })
+    position_id?: string;
+
+    @IsOptional()
+    @IsInt()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 5,
+        description: 'The number of slots for this position',
+    })
+    slots?: number;
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'Manages the development team',
+        description: 'The description of the position'
+    })
+    job_description?: string;
+
+    @IsOptional()
+    @IsUUID()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'Deparment PK UUID',
+        description: 'The PK uuid of the department',
+    })
+    department_id?: string;
+
+    @IsOptional()
+    @IsString()
+    @IsEnum(EmployeeType, { message: 'Employment type must be land_based or sea_based'})
+    @Type(() => String)
+    @ApiProperty({
+        enum: EmployeeType,
+        example: EmployeeType.LAND_BASED,
+        description: 'The employee type of this career posting'
+    })
+    employee_type?: EmployeeType;
+
+    @IsOptional()
+    @IsBoolean()
+    @ApiProperty({
+        example: 'true or false',
+        description: 'true if want to published and false if unpublished'
+    })
+    isPublished: boolean;
+
+    @IsOptional()
+    @IsBoolean()
+    @ApiProperty({
+        example: 'true or false',
+        description: 'true if active and false if set to inactive'
+    })
+    isActive: boolean;
+
+    @IsOptional()
+    @IsString()
+    @IsEnum(EmploymentType, { message: 'Employment type must be full_time or part_time'})
+    @Type(() => String)
+    @ApiProperty({
+        enum: EmploymentType,
+        example: EmploymentType.FULL_TIME,
+        description: 'The employment type of this career posting'
+    })
+    employment_type?: EmploymentType;
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'User Location PK UUID',
+        description: 'The PK uuid of the user location',
+    })
+    user_location_id?: string;
 }
