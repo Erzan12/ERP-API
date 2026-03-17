c6716fef4a1136160e0a58021ebcb3761ba45d49
erzan <earl.do@avegabros.com>
2026-03-17 17:14:46 +0800

    get all applicants and create applicant api added

--- src/modules/hris/hiring-pipeline/controller/hiring-pipelineV2.controller.ts
+++ src/modules/hris/hiring-pipeline/controller/hiring-pipelineV2.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HiringPipelineService } from '../hiring-pipeline.service';
+import { CreateApplicantDto } from '../dto/applicant.dto';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_CREATE, ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { Can } from 'src/utils/decorators/can.decorator';
+import { PaginationDto } from 'src/utils/dtos/pagination.dto';
 
-@Controller('hiring-pipeline')
-export class HiringPipelineV2Controller {}
+@ApiTags('Human Resources - Recruitment and Onboarding')
+@Controller({path: 'hris', version: '2'})
+export class HiringPipelineV2Controller {
+    constructor(private readonly hiringPipelineService: HiringPipelineService) {}
+
+    @Get('hiring-pipeline')
+    @ApiOperation({ summary: 'List of all job/career postings' })
+    @ApiGetResponse('List of employees')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getCareerPostings(
+        @SessionUser() user: RequestUser,
+        @Query() dto: PaginationDto,
+        @Query('page') page = 1,
+        @Query('perPage') perPage = 10,
+        @Query('search') search?: string,
+        @Query('sortBy') sortBy: string = 'created_at',
+        @Query('order') order: 'asc' | 'desc' = 'asc',
+    ) {
+    return this.hiringPipelineService.getApplicants(user,dto);
+    }
+
+    @Post('hiring-pipeline')
+    @ApiBody({
+        type: CreateApplicantDto,
+        description: 'Payload to create Applicant',
+    })
+    @ApiOperation({ summary: 'Applicant posting' })
+    @ApiPostResponse('Applicant posted successfully')
+    @Can({ action: ACTION_CREATE, subject: EMPLOYEE_MASTERLIST })
+    createApplicant(
+        @Body() dto: CreateApplicantDto,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.hiringPipelineService.createApplicant(dto,user)
+    }
+}
--- src/modules/hris/hiring-pipeline/hiring-pipeline.service.ts
+++ src/modules/hris/hiring-pipeline/hiring-pipeline.service.ts
@@ -1,4 +1,267 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { CreateApplicantDto } from './dto/applicant.dto';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { PaginationDto } from 'src/utils/dtos/pagination.dto';
 
 @Injectable()
-export class HiringPipelineService {}
+export class HiringPipelineService {
+    constructor(private prisma: PrismaService) {}
+
+    async getApplicants(
+        user: RequestUser,
+        dto: PaginationDto,
+    ) {
+        const { search, sortBy, order, page, perPage } = dto;
+
+        //pagination area
+        const skip = (page - 1) * perPage;
+
+        const whereCondition: any = {
+            isActive: true,
+        };
+
+        const careerFields = ['name'];
+        const userLocationFields = ['name'];
+
+        let whereConditions: any = {};
+
+        if (search) {
+            whereConditions = {
+                OR: [
+                    ...careerFields.map((field) => ({
+                        careerPosting: {
+                            [field]: {
+                                contains: search,
+                                mode: 'insensitive',
+                            },
+                        },
+                    })),
+                    ...userLocationFields.map((field) => ({
+                        userLocation: {
+                            [field]: {
+                                contains: search,
+                                mode: 'insensitive',
+                            },
+                        },
+                    })),
+                    {
+                       application_source: {
+                        contains: search,
+                        mode: 'insensitive',
+                       },
+                    },
+                    {
+                        application_status: {
+                            contains: search,
+                            mode: 'insensitive',
+                        },
+                    },
+                ],
+            };
+        }
+
+        const allowSortFeilds = [
+            'career_id',
+            'application_source',
+            'applicaiton_status',
+            'date_applied',
+        ];
+
+        if (!allowSortFeilds.includes(sortBy)) {
+            sortBy;
+        }
+
+        const [total, applicants] = await this.prisma.$transaction([
+            this.prisma.applicant.count({
+                where: {
+                    ...whereCondition,
+                    ...whereConditions
+                },
+            }),
+            this.prisma.applicant.findMany({
+                where: {
+                    ...whereCondition,
+                    ...whereConditions,
+                },
+                select: {
+                    id: true,
+                    careerPosting: {
+                        select: {
+                            id: true,
+                            position: {
+                                select: {
+                                    name: true
+                                }
+                            }
+                        },
+                    },
+                    first_name: true,
+                    middle_name: true,
+                    last_name: true,
+                    email: true,
+                    mobile_number: true,
+                    application_source: true,
+                    application_status: true,
+                    date_applied: true,
+                    isActive: true,
+                    created_at: true,
+                    updated_at: true,
+                    createdBy: {
+                        select: {
+                            person: {
+                                select: {
+                                    first_name: true,
+                                    middle_name: true,
+                                    last_name: true,
+                                }
+                            }
+                        }
+                    },
+                    updatedBy: {
+                        select: {
+                            person: {
+                                select: {
+                                    first_name: true,
+                                    middle_name: true,
+                                    last_name: true,
+                                }
+                            }
+                        }
+                    }
+                },
+                skip,
+                take: perPage,
+                orderBy: {
+                    [sortBy]: order,
+                },
+            }),
+        ]);
+
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const allowedRoles = [
+            'Administrator',
+            'Super Administrator',
+            'HR Manager',
+            'HR Clerk',
+            'HR Staff',
+        ];
+
+        const canView = requestUser.user_roles.some((role) =>
+            allowedRoles.includes(role.role_name),
+        );
+
+        if (!canView) {
+            throw new ForbiddenException(
+                'You are not authorized to perform this action',
+            );
+        }
+
+        return {
+            status: 'success',
+            message: 'List of Applicant Posting',
+            count: total,
+            page,
+            perPage,
+            // totalPage: Math.ceil(total / perPage),
+            applicants,
+        };
+    }
+
+    //create applicant
+    async createApplicant(
+        createApplicantDto: CreateApplicantDto,
+        user: RequestUser,
+    ) {
+
+        const { career_id } = createApplicantDto;
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                    include: {
+                        person: true,
+                        position: true,
+                    },
+                },
+                user_roles:true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const allowedRoles = [
+            'Administrator',
+            'Super Administrator',
+            'HR Manager',
+            'HR Clerk',
+            'HR Staff',
+        ];
+
+        const canView = requestUser.user_roles.some((role) =>
+            allowedRoles.includes(role.role_name),
+        );
+
+        if (!canView) {
+            throw new ForbiddenException(
+                'You are not allowed to perform this action',
+            );
+        } 
+
+        await this.prisma.careerPosting.findUnique({
+            where: {
+                id: career_id,
+            },
+            select: {
+                user_location: {
+                    select: {
+                        locationName: true,
+                    }
+                }
+            }
+        });
+
+        const applicant = await this.prisma.applicant.create({
+            data: {
+                career_id: career_id,
+                first_name: createApplicantDto.first_name,
+                middle_name: createApplicantDto.middle_name ?? undefined,
+                last_name: createApplicantDto.last_name,
+                email: createApplicantDto.email,
+                mobile_number: createApplicantDto.mobile_number,
+                application_source: createApplicantDto.application_source,
+                application_status: createApplicantDto.application_status,
+                date_applied: new Date(),
+                created_by: user.id,
+            }
+        })
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPosition = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: `Applicant has been created successfully`,
+            applicant,
+            created_by_user: `${userName} - ${userPosition}`
+        }
+    }
+}
