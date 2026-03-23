106028b196050852f6bfb03f625c735eeb8ae5b1
erzan <earl.do@avegabros.com>
2026-03-23 11:23:10 +0800

    fix not null migration error

---
+++ prisma/migrations/20260323032003_make_user_location_nullable/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Made the column `location_name` on table `UserLocation` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "ExaminationRating" ADD COLUMN     "result" TEXT;
+
+-- AlterTable
+ALTER TABLE "UserLocation" ALTER COLUMN "location_name" SET NOT NULL;
