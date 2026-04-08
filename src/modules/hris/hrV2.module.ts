6def7da62b3cf819b1045b826b10b2a17de5129d
erzan12 <do.earljan@gmail.com>
2026-04-08 15:29:36 +0800

    updated module directories

--- src/modules/hris/hrV2.module.ts
+++ src/modules/hris/hrV2.module.ts
@@ -21,6 +21,8 @@ import {
   ApplicantsController,
   InterviewApplicantController,
 } from './hiring-pipeline/hiring-pipelineV2.controller';
+import { RegularizationReviewsController } from './regularization_reviews/regularization_reviews.controller';
+import { RegularizationReviewsService } from './regularization_reviews/regularization_reviews.service';
 
 @Module({
   imports: [AuthModule, AdministratorV2Module],
@@ -31,6 +33,7 @@ import {
     CareerPostingService,
     HiringPipelineService,
     InterviewApplicantService,
+    RegularizationReviewsService,
     // ScreeningApplicantService,
   ],
   controllers: [
@@ -40,6 +43,7 @@ import {
     ApplicantsController,
     // ScreeningApplicantController,
     InterviewApplicantController,
+    RegularizationReviewsController,
   ],
   exports: [HrV2Module],
 })
