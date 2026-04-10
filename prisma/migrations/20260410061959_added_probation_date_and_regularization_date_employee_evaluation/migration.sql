76f890950ef973b8c6f73dc90ce9c44e70bbc1e1
erzan12 <do.earljan@gmail.com>
2026-04-10 15:27:06 +0800

    added new schema migration for regularization reviews, and dto

---
+++ prisma/migrations/20260410061959_added_probation_date_and_regularization_date_employee_evaluation/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "EmployeeEvaluation" ADD COLUMN     "probation_date" TIMESTAMP(3),
+ADD COLUMN     "regularization_date" TIMESTAMP(3);
--- src/modules/hris/performance_management/regularization_reviews/regularization_reviews.controller.ts
+++ src/modules/hris/performance_management/regularization_reviews/regularization_reviews.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Session } from '@nestjs/common';
+import { Body, Controller, Get, Post, Session } from '@nestjs/common';
 import { RegularizationReviewsService } from './regularization_reviews.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
 import { Can } from 'src/utils/decorators/can.decorator';
-import { ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { ACTION_CREATE, ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateEvaluationDto } from './dto/evaluation.dto';
 
 @ApiTags('Human Resources - Performance Management (Regularization Reviews)')
 @Controller({path:'regularization-reviews', version: '2'})
@@ -20,5 +21,16 @@ export class RegularizationReviewsController {
         @SessionUser() user: RequestUser,
     ) {
         return this.regularizationService.getForRegularization(user);
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'Create an Employee Evaluation for the first 3rd and 5th month' })
+    @ApiPostResponse('Evaluation successfully created')
+    @Can({ action: ACTION_CREATE, subject: EMPLOYEE_MASTERLIST })
+    createEvaluation(
+        @SessionUser() user: RequestUser,
+        @Body() dto: CreateEvaluationDto,
+    ) {
+        return this.regularizationService.createEvaluation(dto,user)
     }
 }
