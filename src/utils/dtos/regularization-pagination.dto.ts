e684585a0a56c6cf34348240d8a902f913e37cec
erzan12 <do.earljan@gmail.com>
2026-04-13 16:30:38 +0800

    chore(regularization-review): added new regularization pagination dto

---
+++ src/utils/dtos/regularization-pagination.dto.ts
@@ -0,0 +1,51 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import { IsBoolean, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class RecruitmentPaginationDto {
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ default: '' })
+  search?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ default: '' })
+  status?: string;
+
+  // @IsOptional()
+  // @IsBoolean()
+  // @Transform(({ value }) => value === 'true')
+  // @ApiPropertyOptional({ default: '' })
+  // is_active?: boolean;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ example: 'id', default: 'id' })
+  sortBy: string = 'id';
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ example: 'desc', default: 'desc' })
+  order: 'asc' | 'desc' = 'asc';
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @ApiPropertyOptional({ example: 1, default: 1 })
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @ApiPropertyOptional({ example: 10, default: 10 })
+  perPage: number = 10;
+}
+
+// export class StatusCountDto {
+//   @IsOptional()
+//   @IsBoolean()
+//   @Transform(({ value }) => value === 'true')
+//   @ApiPropertyOptional({ default: '' })
+//   is_active?: boolean;
+// }
