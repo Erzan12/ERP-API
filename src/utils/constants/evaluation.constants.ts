d8e35f24187a143a6e67a66416b976be4239beee
erzan12 <do.earljan@gmail.com>
2026-04-13 10:29:34 +0800

    feat(constant): added evaluation contants for performance mgnt module

---
+++ src/utils/constants/evaluation.constants.ts
@@ -0,0 +1,10 @@
+export const STAGE_RULES = {
+  third_month_evaluation: 3,
+  fifth_month_evaluation: 5,
+  regularization_evaluation: 6,
+};
+
+export const PREVIOUS_STAGE_MAP = {
+  third_month_evaluation: 'third_month_evaluation',
+  fifth_month_evaluation: 'fifth_month_evaluation',
+};
