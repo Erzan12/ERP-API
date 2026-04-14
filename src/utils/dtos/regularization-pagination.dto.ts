a7c877955c052c9be21f64dd3898ebce2061b7ca
erzan12 <do.earljan@gmail.com>
2026-04-14 14:11:34 +0800

    refactor(dto): copied and rename to regularization review dto

--- src/utils/dtos/regularization-pagination.dto.ts
+++ src/utils/dtos/regularization-pagination.dto.ts
@@ -1,8 +1,8 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Transform, Type } from 'class-transformer';
-import { IsBoolean, IsInt, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
-export class RecruitmentPaginationDto {
+export class RegularizationReviewDto {
   @IsOptional()
   @IsString()
   @ApiPropertyOptional({ default: '' })
