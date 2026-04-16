505aefa98fa3e7ce59126a586e3f71ebd4eb35ab
erzan12 <do.earljan@gmail.com>
2026-04-16 15:44:23 +0800

    feat(api): added new getMyEvaluations api to Corp Serv Module

--- src/modules/corporate_services/performance_evaluations/performance_evaluations.controller.ts
+++ src/modules/corporate_services/performance_evaluations/performance_evaluations.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Session } from '@nestjs/common';
+import { PerformanceEvaluationService } from './performance_evaluations.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { Can } from 'src/utils/decorators/can.decorator';
 
-@Controller('evaluator')
-export class PerformanceEvaluationController {}
+@ApiTags('Corporate Services - Performance Evaluation')
+@Controller({path: 'corporate-services', version: '2'})
+export class PerformanceEvaluationController {
+    constructor (private readonly performanceEvaluationService: PerformanceEvaluationService) {}
+
+    @Get('performance-evaluation/my-evalations')
+    @ApiOperation({ summary: 'List of evaluations added to this employee/user' })
+    @ApiGetResponse('List of my evaluations')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getMyEvaluations(
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.performanceEvaluationService.getMyEvaluations(user)
+    }
+}
