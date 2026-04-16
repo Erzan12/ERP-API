d74a582bbf9eedbb59112cda84fb91661bc9c6dc
erzan12 <do.earljan@gmail.com>
2026-04-16 15:44:23 +0800

    feat(api): added new getMyEvaluations api to Corp Serv Module

--- src/modules/corporate_services/performance_evaluations/performance_evaluations.service.ts
+++ src/modules/corporate_services/performance_evaluations/performance_evaluations.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { RequestUser } from 'src/utils/types/request-user.interface';
 
 @Injectable()
-export class PerformanceEvaluationService {}
+export class PerformanceEvaluationService {
+    constructor (private readonly prisma: PrismaService) {}
+
+    async getMyEvaluations(user: RequestUser) {
+
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            select: {
+                employee: {
+                select: {
+                    id: true,
+                },
+                },
+            },
+        });
+
+        const myEvaluations = await this.prisma.employeeEvaluation.findMany({
+            where: {
+                evaluator_id: requestUser?.employee?.id,
+            },
+        });
+
+        return {
+            myEvaluations,
+        };
+    }
+}
