3677f48a78b655823f09f343f65eb58f6a5cc9d5
erzan12 <do.earljan@gmail.com>
2026-04-20 11:00:37 +0800

    feat(performance-comp): added user validator check on get and create

--- src/modules/hris/performance_management/performance_competency/performance_competency.service.ts
+++ src/modules/hris/performance_management/performance_competency/performance_competency.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 import { CreatePerformanceCompetencyDto } from './dto/performance_comtency.dto';
@@ -8,6 +8,19 @@ export class PerformanceCompetencyService {
     constructor(private readonly prisma: PrismaService) {} 
 
     async getCompetencies(user: RequestUser) {
+        // Authorization Check
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: { user_roles: true, employee: true }
+        });
+
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
         const competencies = await this.prisma.hrPerformanceCompetency.findMany()
     
         if (competencies.length === 0) {
@@ -23,6 +36,19 @@ export class PerformanceCompetencyService {
 
     async createCompetencies(user: RequestUser, dto: CreatePerformanceCompetencyDto) {
         const { department_group, sea_category, land_category, title, description, highest_score_limit, performanceRating } = dto;
+
+        // Authorization Check
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: { user_roles: true, employee: true }
+        });
+
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
 
         const competency = await this.prisma.hrPerformanceCompetency.create({
             data: {
@@ -42,4 +68,6 @@ export class PerformanceCompetencyService {
             competency
         }
     }
+
+
 }
