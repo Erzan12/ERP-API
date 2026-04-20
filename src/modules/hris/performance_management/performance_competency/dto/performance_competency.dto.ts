e3e7299831104876ed3e4de6623e83a29c460c46
erzan12 <do.earljan@gmail.com>
2026-04-20 11:26:31 +0800

    feat(perf-comp): added new update api
    For Performance Competency module

--- src/modules/hris/performance_management/performance_competency/dto/performance_competency.dto.ts
+++ src/modules/hris/performance_management/performance_competency/dto/performance_competency.dto.ts
@@ -82,3 +82,81 @@ export class CreatePerformanceCompetencyDto {
     })
     performanceRating?: PerformanceRating;
 }
+
+export class UpdatePerformanceCompetencyDto {
+
+    @IsOptional()
+    @Validate(CategoryMatchValidator)
+    private readonly _categoryCheck: any;
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsEnum(EmployeeType, {
+        message: 'Valid inputs are: land_based and sea_based'
+        // message: 'If selected Land based group valid inputs are: rank_and_file and managerial_and_supervisory. If selected Sea based group valid inputs are: all_ranks, all_officers and top_2_master_chief_engineer.'
+    })
+    @Type(() => String)
+    @ApiProperty({
+        enum: EmployeeType,
+        example: EmployeeType.LAND_BASED,
+        description: 'The deparment group of the employee'
+    })
+    department_group?: EmployeeType;
+
+    @IsOptional()
+    @IsEnum(SeaBasedCategory, {
+        message: 'Valid sea categories: all_ranks, all_officers, top_2_master_chief_engineer'
+    })
+    @ApiProperty({
+        enum: SeaBasedCategory,
+        required: false
+    })
+    sea_category?: SeaBasedCategory;
+
+    @IsOptional()
+    @IsEnum(LandBasedCategory, {
+        message: 'Valid land categories: rank_and_file, managerial_and_supervisory'
+    })
+    @ApiProperty({
+        enum: LandBasedCategory,
+        required: false
+    })
+    land_category?: LandBasedCategory;
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        example: 'Name or Title of the performance competency',
+        description: 'The name of the performance competency'
+    })
+    title?: string;
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        example: 'Detail or description of this performance competency',
+        description: 'Add description for this performance competence'
+    })
+    description?: string;
+
+    @IsInt()
+    @IsOptional()
+    @ApiProperty({
+        example: 'Rate from 1 to 5, 5 is highest 1 is lowest',
+        description: 'Highest rate of this performance competency'
+    })
+    highest_score_limit?: number;
+
+    @IsOptional()
+    @IsEnum(PerformanceRating, {
+        message: 'Valid inputs are: unsatisfactory, needs_improvement, meets_expectations, exceed_expectations, exceptional'
+        // message: 'If selected Land based group valid inputs are: rank_and_file and managerial_and_supervisory. If selected Sea based group valid inputs are: all_ranks, all_officers and top_2_master_chief_engineer.'
+    })
+    @Type(() => String)
+    @ApiProperty({
+        enum: PerformanceRating,
+        example: PerformanceRating.exceptional,
+        description: 'Highest rate of this performance competence'
+    })
+    performanceRating?: PerformanceRating;
+}
