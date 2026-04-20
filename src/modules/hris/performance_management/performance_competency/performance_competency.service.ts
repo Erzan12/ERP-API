56fc84760e810d9170f9ea19bdc41b1fe66da073
erzan12 <do.earljan@gmail.com>
2026-04-20 11:26:31 +0800

    feat(perf-comp): added new update api
    For Performance Competency module

--- src/modules/hris/performance_management/performance_competency/performance_competency.service.ts
+++ src/modules/hris/performance_management/performance_competency/performance_competency.service.ts
@@ -1,7 +1,7 @@
-import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
-import { CreatePerformanceCompetencyDto } from './dto/performance_comtency.dto';
+import { CreatePerformanceCompetencyDto, UpdatePerformanceCompetencyDto } from './dto/performance_competency.dto';
 
 @Injectable()
 export class PerformanceCompetencyService {
@@ -13,6 +13,10 @@ export class PerformanceCompetencyService {
             where: { id: user.id },
             include: { user_roles: true, employee: true }
         });
+
+        if (!requestUser || !requestUser.employee) {
+            throw new BadRequestException(`User does not exist.`);
+        }
 
         const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
         const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
@@ -43,6 +47,10 @@ export class PerformanceCompetencyService {
             include: { user_roles: true, employee: true }
         });
 
+        if (!requestUser || !requestUser.employee) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
         const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
         const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
 
@@ -69,5 +77,61 @@ export class PerformanceCompetencyService {
         }
     }
 
+    async updateCompetency(competencyId: string, user: RequestUser, dto: UpdatePerformanceCompetencyDto) {
+        // Authorization Check
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: { 
+                user_roles: true,
+                employee: { 
+                    include: { 
+                        person: true, 
+                        position: true 
+                    } 
+                } 
+            }
+        });
 
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const existingCompetency = await this.prisma.hrPerformanceCompetency.findUnique({
+            where: { id: competencyId }
+        });
+
+        if (!existingCompetency) {
+            throw new NotFoundException('Competency not found');
+        }
+
+        const competency = await this.prisma.hrPerformanceCompetency.update({
+            where: { id: competencyId },
+            data: {
+                department_group: dto.department_group ?? undefined,
+                sea_category: dto.sea_category ?? undefined,
+                land_category: dto.land_category ?? undefined,
+                title: dto.title ?? undefined,
+                description: dto.description ?? undefined,
+                highest_score_limit: dto.highest_score_limit ?? undefined,
+                performanceRating: dto.performanceRating ?? undefined,
+            },
+        });
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPosition = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: `Performance Competency has been updated successfully!`,
+            competency,
+            updated_by_user: `${userName} - ${userPosition}`,
+        }
+    }
 }
