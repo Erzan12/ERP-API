9c3429e20f6ab0f6ed46f32fe7f3b8febbdd3c86
erzan12 <do.earljan@gmail.com>
2026-04-20 16:10:17 +0800

    feat(submit-eval): added new api to submit evaluation from evaluator

--- src/modules/employee_dashboard/performance_evaluations/performance_evaluations.service.ts
+++ src/modules/employee_dashboard/performance_evaluations/performance_evaluations.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
+import { SubmitEvaluationDto } from './dto/performance_evaluation.dto';
 
 @Injectable()
 export class PerformanceEvaluationService {
@@ -55,5 +56,77 @@ export class PerformanceEvaluationService {
             message: 'List of Employees I will evaluate',
             toBeEvaluated,
         };
+    }
+
+    async submitEvaluation(evaluationId: string, user: RequestUser, dto: SubmitEvaluationDto) {
+        
+        // Authorization Check
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: { user_roles: true, employee: true }
+        });
+
+        if (!requestUser || !requestUser.employee) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+        
+        const existingEvaluation = await this.prisma.hrEmployeeEvaluation.findFirst({
+            where: { id: evaluationId }
+        })
+
+        if (!existingEvaluation) {
+            throw new NotFoundException('Evaluation not found or does not exist')
+        }
+
+        await this.prisma.$transaction(async (tx) => {
+        // Delete old details (if re-submitting)
+        await tx.hrPerformanceEvaluationDetails.deleteMany({
+            where: { evaluation_id: evaluationId },
+        });
+
+        // Insert new details
+        await tx.hrPerformanceEvaluationDetails.createMany({
+                data: dto.details.map(d => ({
+                evaluation_id: evaluationId,
+                competency_id: d.competency_id,
+                rating: d.rating,
+                remarks: d.remarks,
+            })),
+        });
+
+        // Compute overall rating
+        const map = {
+            unsatisfactory: 1,
+            needs_improvement: 2,
+            meets_expectations: 3,
+            exceed_expectations: 4,
+            exceptional: 5,
+        };
+
+        const scores = dto.details.map(d => map[d.rating]);
+        const overall = Math.round(
+            scores.reduce((a, b) => a + b, 0) / scores.length
+        );
+
+        // Update evaluation
+        await tx.hrEmployeeEvaluation.update({
+                where: { id: evaluationId },
+                data: {
+                    overall_rating: overall,
+                    decision: dto.decision,
+                    comments: dto.comments,
+                    evaluated_on: new Date(),
+                    updated_by: user.id
+                    // status: 'evaluated',
+                },
+            });
+        });        
     }
 }
