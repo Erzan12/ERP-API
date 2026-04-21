b984bc07e005ee7716b15de5675ee9b4261d5498
erzan12 <do.earljan@gmail.com>
2026-04-21 11:16:45 +0800

    feat(performance-eval): new module performance eval

--- src/modules/employee-dashboard/employee-dashboard.module.ts
+++ src/modules/employee-dashboard/employee-dashboard.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PerformanceEvaluationController } from './performance-evaluation/performance-evaluation.controller';
 import { PerformanceEvaluationService } from './performance-evaluation/performance-evaluation.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaService } from 'src/config/prisma/prisma.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PerformanceEvaluationController],
-  providers: [PerformanceEvaluationService]
+  providers: [PerformanceEvaluationService, PrismaService]
 })
 export class EmployeeDashboardModule {}
