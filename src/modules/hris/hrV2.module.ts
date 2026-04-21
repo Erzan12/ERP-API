85fdb54c8b1aa643dab622d9e30a93a9f383440d
erzan12 <do.earljan@gmail.com>
2026-04-21 11:30:19 +0800

    refactor(hris): move hris module directories

--- src/modules/hris/hrV2.module.ts
+++ src/modules/hris/hrV2.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 
-import { EmployeeService } from './employee/employee_masterlist/employee.service';
-import { EmployeeControllerV2 } from './employee/employee_masterlist/employee.controller';
+import { EmployeeService } from './employee/employee-masterlist/employee.service';
+import { EmployeeControllerV2 } from './employee/employee-masterlist/employee.controller';
 
 import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardControllerV2 } from './dashboard/dashboardV2.controller';
@@ -10,17 +10,17 @@ import { DashboardControllerV2 } from './dashboard/dashboardV2.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { AdministratorV2Module } from '../administrator/administratorV2.module';
 
-import { CareerPostingService } from './career-posting/career-posting.service';
-import { CareerPostingV2Controller } from './career-posting/career-posting-v2.controller';
+import { CareerPostingService } from './recruitment_and_onboarding/career-posting/career-posting.service';
+import { CareerPostingV2Controller } from './recruitment_and_onboarding/career-posting/career-posting-v2.controller';
 
 import {
   HiringPipelineService,
   InterviewApplicantService,
-} from './hiring-pipeline/hiring-pipeline.service';
+} from './recruitment_and_onboarding/hiring-pipeline/hiring-pipeline.service';
 import {
   ApplicantsController,
   InterviewApplicantController,
-} from './hiring-pipeline/hiring-pipelineV2.controller';
+} from './recruitment_and_onboarding/hiring-pipeline/hiring-pipelineV2.controller';
 
 @Module({
   imports: [AuthModule, AdministratorV2Module],
