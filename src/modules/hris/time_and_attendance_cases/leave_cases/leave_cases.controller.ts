966c4bfd78be0783aa13d4768e868b6ea86ae8e0
erzan12 <do.earljan@gmail.com>
2026-04-27 11:24:38 +0800

    feat(api): added new api for leave cases list: get leave cases api

--- src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.controller.ts
+++ src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { LeaveCasesService } from './leave_cases.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { Can } from 'src/utils/decorators/can.decorator';
+import { ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
 
-@Controller('leave-cases')
-export class LeaveCasesController {}
+@ApiTags('Human Resources - Time and Attendance Cases (Leave Cases)')
+@Controller({path:'hris', version: '2'})
+export class LeaveCasesController {
+    constructor (private readonly leaveCasesService: LeaveCasesService) {}
+
+    @Get('time-and-attendance-cases/leave-cases')
+    @ApiOperation({ summary: 'List of all Leave Cases' })
+    @ApiGetResponse('List of Leave Cases')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getLeaveCases(
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCasesService.getLeaveCases(user)
+    }
+}
