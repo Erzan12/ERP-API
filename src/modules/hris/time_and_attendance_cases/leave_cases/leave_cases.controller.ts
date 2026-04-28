f3b57e630c82e6879e87bfca1d1fac0125453c35
erzan12 <do.earljan@gmail.com>
2026-04-28 10:57:40 +0800

    feat(api): new controller and api for leave cases
    request leave and submitleave

--- src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.controller.ts
+++ src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { LeaveCasesService } from './leave_cases.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
 import { Can } from 'src/utils/decorators/can.decorator';
 import { ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateLeaveRequestWithDetailsDto } from './dto/leave-case.dto';
 
 @ApiTags('Human Resources - Time and Attendance Cases (Leave Cases)')
 @Controller({path:'hris', version: '2'})
@@ -20,5 +21,20 @@ export class LeaveCasesController {
         @SessionUser() user: RequestUser,
     ) {
         return this.leaveCasesService.getLeaveCases(user)
+    }
+
+    @Post('time-and-attendance-cases/leave-cases')
+    @ApiBody({
+        type: CreateLeaveRequestWithDetailsDto,
+        description: 'Payload to create Leave Request'
+    })
+    @ApiOperation({ summary: 'Leave Request Creation' })
+    @ApiPostResponse('Leave Request successfully created')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    createLeaveRequest(
+        @Body() dto: CreateLeaveRequestWithDetailsDto,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCasesService.createLeaveCase(user, dto)
     }
 }
--- src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.service.ts
+++ src/modules/hris/time_and_attendance_cases/leave_cases/leave_cases.service.ts
@@ -1,6 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateLeaveRequestWithDetailsDto } from './dto/leave-case.dto';
+import { WORKFLOW_ENTITY } from 'src/utils/constants/workflow-entity.constants';
 
 @Injectable()
 export class LeaveCasesService {
@@ -18,5 +21,149 @@ export class LeaveCasesService {
             message: 'List of Leave Cases',
             leaves
         }
+    }
+
+    async createLeaveCase(
+        user: RequestUser,
+        dto: CreateLeaveRequestWithDetailsDto
+    ) {
+        const { leave_request, leave_dates } = dto
+
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const existingEmployee = await this.prisma.employee.findUnique({
+            where: { id: leave_request.employee_id }
+        })
+
+        if (!existingEmployee) {
+            throw new NotFoundException('Employee does not exist')
+        }
+
+        if(leave_dates.length === 0) {
+            throw new BadRequestException("Leave dates cannot be empty");
+        }
+
+        const from = new Date(leave_request.date_from);
+        const to = new Date(leave_request.date_to);
+
+        for (const d of leave_dates) {
+        const date = new Date(d.leave_date);
+
+            if (date < from || date > to) {
+                throw new BadRequestException(
+                `Leave date ${d.leave_date} is outside range`
+                );
+            }
+        }
+
+        const uniqueDates = new Set(
+            leave_dates.map(d => d.leave_date)
+        );
+
+        if (uniqueDates.size !== leave_dates.length) {
+            throw new BadRequestException("Duplicate leave dates detected");
+        }
+
+        const no_of_days = leave_dates.reduce(
+            (sum, d) => sum + (d.fraction ?? 1),
+            0
+        );
+
+        return this.prisma.$transaction(async (tx) => {
+            const leaveRequest = await this.prisma.hrLeaveRequest.create({
+                data: {
+                    employee_id: leave_request.employee_id,
+                    date_from: new Date(leave_request.date_from),
+                    date_to: new Date(leave_request.date_to),
+                    reason: leave_request.reason,
+                    contact_number: leave_request.contact_number,
+                    address_on_leave: leave_request.address_on_leave,
+                    no_of_days: no_of_days,
+                    reliever_id: leave_request.reliever_id,
+                    verifier_id: leave_request.verifier_id,
+                    approver_id: leave_request.approver_id,
+                    created_by: requestUser.id,
+                    date_created: new Date(),
+
+                    hr_leave_dates: {
+                        create: leave_dates.map(d => ({
+                            leave_date: new Date(d.leave_date),
+                            category: {
+                                connect: { id: d.leave_type }
+                            },
+                            fraction: d.fraction ?? 1.0,
+                        })),
+                    }
+                },
+                include: {
+                    hr_leave_dates: true
+                }
+            });
+
+            // ADD WORKFLOW ACTION
+            await tx.workflowAction.create({
+                data: {
+                    actionable_type: WORKFLOW_ENTITY.LEAVE_REQUEST,
+                    actionable_id: leaveRequest.id,
+                    action: "draft",
+                    acted_by: requestUser.id
+                }
+            });
+
+            const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+            const userPosition = requestUser.employee.position.name;
+
+            return {
+                status: 'success',
+                message: 'Leave Request Createad',
+                leaveRequest,
+                created_by: `${userName} - ${userPosition}`,
+            };
+        });
+    }
+
+    async submitLeave(hrLeaveRequestId: string, user: RequestUser) {
+        return this.prisma.$transaction(async (tx) => {
+
+            await tx.hrLeaveRequest.update({
+                where: { id: hrLeaveRequestId },
+                data: {
+                    status: "for_verification",
+                    date_verified: new Date()
+                }
+            });
+
+            await tx.workflowAction.create({
+                data: {
+                    actionable_type: "LeaveRequest",
+                    actionable_id: hrLeaveRequestId,
+                    action: "submitted",
+                    acted_by: user.id
+                }
+            });
+        });
     }
 }
