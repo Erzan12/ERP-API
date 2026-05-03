986f467929da5e8eed0c835afb3635badc4171a7
erzan <do.earljan@gmail.com>
2026-05-03 08:38:34 +0800

    feat: new crud api for leave category module

--- src/modules/hris/time-and-attendance-cases/leave-category/leave-category.controller.ts
+++ src/modules/hris/time-and-attendance-cases/leave-category/leave-category.controller.ts
@@ -1,4 +1,72 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { LeaveCategoryService } from './leave-category.service';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse, ApiPatchResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_READ, ACTION_UPDATE, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { Can } from 'src/utils/decorators/can.decorator';
+import { CreateLeaveCategory, UpdateLeaveCategory } from './dto/leave-category.dto';
 
-@Controller('leave-category')
-export class LeaveCategoryController {}
+@ApiTags("Human Resources - Time and Attendance Cases (Leave Category)")
+@Controller({path: 'hris', version: '2' })
+export class LeaveCategoryController {
+    constructor (
+        private readonly leaveCategoryService: LeaveCategoryService
+    ) {}
+
+    @Get('time-and-attendance-cases/leave-category')
+    @ApiOperation({ summary: 'List of all Leave Categories'})
+    @ApiGetResponse('List of Leave Categories')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getLeaveCategories(
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCategoryService.getLeaveCategories(user)
+    }
+
+    @Get('time-and-attendance-cases/leave-category/:leaveCategoryId')
+    @ApiOperation({ summary: 'Get a Leave Category' })
+    @ApiGetResponse('Here is the Leave Category')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getLeaveCategory(
+        @Param('leaveCategoryId', new ParseUUIDPipe()) leaveCategoryId: string,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCategoryService.getLeaveCategory(leaveCategoryId, user)
+    }
+
+    @Post('time-and-attendance-cases/leave-category')
+    @ApiBody({
+        type: CreateLeaveCategory,
+        description: 'Payload to create Leave Category'
+    })
+    @ApiOperation({ summary: 'Leave Category creation' })
+    @ApiPostResponse('Leave Category successfully created')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    createLeaveCategory(
+        @Body() dto: CreateLeaveCategory,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCategoryService.createLeaveCategory(user, dto)
+    }
+
+    @Put('time-and-attendance-cases/leave-category/:leaveCategoryId')
+    @ApiBody({
+        type: UpdateLeaveCategory,
+        description: 'Payload to update leave category'
+    })
+    @ApiOperation({ summary: 'Update a current leave category' })
+    @ApiPatchResponse('Leave Category updated successfully')
+    @Can({ action: ACTION_UPDATE, subject: EMPLOYEE_MASTERLIST })
+    updateLeaveCategory(
+        @Param('leaveCategoryId', new ParseUUIDPipe()) leaveCategoryId: string,
+        @Body() dto: UpdateLeaveCategory,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.leaveCategoryService.updateLeaveCategory(
+            leaveCategoryId,
+            user,
+            dto)
+    }
+}
--- src/modules/hris/time-and-attendance-cases/leave-category/leave-category.service.ts
+++ src/modules/hris/time-and-attendance-cases/leave-category/leave-category.service.ts
@@ -1,4 +1,177 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateLeaveCategory, UpdateLeaveCategory } from './dto/leave-category.dto';
 
 @Injectable()
-export class LeaveCategoryService {}
+export class LeaveCategoryService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getLeaveCategories (user: RequestUser) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: { user_roles: true }
+        });
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const leaveCategories = await this.prisma.hrLeaveCategory.findMany({
+            where: { is_active: true }
+        })
+
+        if (leaveCategories.length === 0) {
+            throw new NotFoundException ("No leave categories found")
+        }
+
+        return {
+            status: 'success',
+            message: 'List of Leave Categories',
+            leaveCategories
+        }
+    }
+
+    async getLeaveCategory (leaveCategoryId: string, user: RequestUser) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const leaveCategory = await this.prisma.hrLeaveCategory.findUnique({
+            where: { id: leaveCategoryId, is_active: true }
+        })
+
+        if (!leaveCategory) {
+            throw new NotFoundException('Leave Category does not exist')
+        }
+
+        return {
+            status: 'success',
+            message: 'Here is the Leave Category',
+            leaveCategory
+        }
+    }
+
+    async createLeaveCategory (user: RequestUser, dto: CreateLeaveCategory) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const existingCategory = await this.prisma.hrLeaveCategory.findFirst({
+            where: { category_name: dto.category_name }
+        })
+
+        if (existingCategory) {
+            throw new ConflictException('Leave Category already exist')
+        }
+
+        const leaveCategory = await this.prisma.hrLeaveCategory.create({
+            data: {
+                category_name: dto.category_name,
+                created_by: user.id,
+            }
+        })
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPosition = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: 'Leave Category created',
+            leaveCategory,
+            created_by: `${userName} - ${userPosition}`,
+        }
+    }
+
+    async updateLeaveCategory (leaveCategoryId: string, user: RequestUser, dto: UpdateLeaveCategory) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+        
+        const leaveCategory = await this.prisma.hrLeaveCategory.update({
+            where: { id: leaveCategoryId },
+            data: {
+                category_name: dto.category_name ?? undefined,
+                is_active: dto.is_active ?? undefined,
+                updated_by: user.id
+            }
+        })
+
+        if (!leaveCategory) {
+            throw new NotFoundException ('Leave category does not exist')
+        }
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPosition = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: 'Leave Category updated',
+            leaveCategory,
+            updated_by_user: `${userName} - ${userPosition}`,
+        }
+    }
+}
