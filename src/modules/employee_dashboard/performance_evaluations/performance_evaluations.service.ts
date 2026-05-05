9eed81a3a05a9ec6d9adee91c509e0711ad25558
erzan12 <do.earljan@gmail.com>
2026-05-05 11:01:23 +0800

    feat: added prisma transaction and try catch in performance eval api

--- src/modules/employee_dashboard/performance_evaluations/performance_evaluations.service.ts
+++ src/modules/employee_dashboard/performance_evaluations/performance_evaluations.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 import { AcknowledgeEvaluationDto, SubmitEvaluationDto } from './dto/performance_evaluation.dto';
 import { Request } from 'express';
+import { WORKFLOW_ENTITY } from 'src/utils/constants/workflow-entity.constants';
 
 @Injectable()
 export class PerformanceEvaluationService {
@@ -96,12 +97,20 @@ export class PerformanceEvaluationService {
         };
     }
 
-    async submitEvaluation(evaluationId: string, user: RequestUser, dto: SubmitEvaluationDto) {
+    async submit(evaluationId: string, user: RequestUser, dto: SubmitEvaluationDto) {
         
         // Authorization Check
         const requestUser = await this.prisma.user.findUnique({
             where: { id: user.id },
-            include: { user_roles: true, employee: true }
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
         });
 
         if (!requestUser || !requestUser.employee) {
@@ -123,59 +132,89 @@ export class PerformanceEvaluationService {
             throw new NotFoundException('Evaluation not found or does not exist')
         }
 
-        await this.prisma.$transaction(async (tx) => {
-        // Delete old details (if re-submitting)
-        await tx.hrPerformanceEvaluationDetails.deleteMany({
-            where: { evaluation_id: evaluationId },
-        });
+        return this.prisma.$transaction(async (tx) => {
+            try {
+                // Delete old details (if re-submitting)
+                await tx.hrPerformanceEvaluationDetails.deleteMany({
+                    where: { evaluation_id: evaluationId },
+                });
 
-        // Insert new details
-        await tx.hrPerformanceEvaluationDetails.createMany({
-                data: dto.details.map(d => ({
-                evaluation_id: evaluationId,
-                competency_id: d.competency_id,
-                rating: d.rating,
-                remarks: d.remarks,
-            })),
-        });
+                // Insert new details
+                await tx.hrPerformanceEvaluationDetails.createMany({
+                        data: dto.details.map(d => ({
+                        evaluation_id: evaluationId,
+                        competency_id: d.competency_id,
+                        rating: d.rating,
+                        remarks: d.remarks,
+                    })),
+                });
 
-        // Compute overall rating
-        const map = {
-            unsatisfactory: 1,
-            needs_improvement: 2,
-            meets_expectations: 3,
-            exceed_expectations: 4,
-            exceptional: 5,
-        };
+                // Compute overall rating
+                const map = {
+                    unsatisfactory: 1,
+                    needs_improvement: 2,
+                    meets_expectations: 3,
+                    exceed_expectations: 4,
+                    exceptional: 5,
+                };
 
-        const scores = dto.details.map(d => map[d.rating]);
-        const overall = Math.round(
-            scores.reduce((a, b) => a + b, 0) / scores.length
-        );
+                const scores = dto.details.map(d => map[d.rating]);
+                const overall = Math.round(
+                    scores.reduce((a, b) => a + b, 0) / scores.length
+                );
 
-        // Update evaluation
-        await tx.hrEmployeeEvaluation.update({
-                where: { id: evaluationId },
-                data: {
-                    overall_rating: overall,
-                    decision: dto.decision,
-                    comments: dto.comments,
-                    evaluated_on: new Date(),
-                    updated_by: user.id
-                    // status: 'evaluated',
-                },
-            });
+                // Update evaluation
+                const submitEvaluation = await tx.hrEmployeeEvaluation.update({
+                    where: { id: evaluationId },
+                    data: {
+                        status: "for_acknowledgment",
+                        overall_rating: overall,
+                        decision: dto.decision,
+                        comments: dto.comments,
+                        evaluated_on: new Date(),
+                        updated_by: user.id,
+                        completed_at: new Date(),
+                        // status: 'evaluated',
+                    },
+                });
+
+                await tx.workflowAction.create({
+                    data: {
+                        actionable_type: WORKFLOW_ENTITY.EMPLOYEE_EVALUATION,
+                        actionable_id: evaluationId,
+                        action: "submit",
+                        acted_by: requestUser.id
+                    }
+                })
+
+                const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+                const userPosition = requestUser.employee.position.name;
+
+                return {
+                    status: 'success',
+                    message: 'Employee Evaluation Verified',
+                    submitEvaluation,
+                    verified_by: `${userName} - ${userPosition}`,
+                };
+            } catch (e) {
+                throw e;
+            }
         });        
     }
 
-    async acknowledgeEvaluation(user: RequestUser, evaluationId: string, dto: AcknowledgeEvaluationDto) {
+    async acknowledge(user: RequestUser, evaluationId: string, dto: AcknowledgeEvaluationDto) {
         // Authorization Check
         const requestUser = await this.prisma.user.findUnique({
             where: { id: user.id },
-            include: { 
-                user_roles: true, 
-                employee: true,
-            }
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
         });
 
         if (!requestUser || !requestUser.employee || !requestUser.employee.id) {
@@ -189,28 +228,50 @@ export class PerformanceEvaluationService {
             throw new ForbiddenException('You are not authorized to perform this action');
         }
 
-        const acknowledgeEvaluation = await this.prisma.hrEmployeeEvaluation.update({
-            where: { 
-                id: evaluationId, 
-                employee_id: requestUser.employee.id , 
-                evaluated_on: { not: null }, 
-                type_of_evaluation: { not: null },
-                overall_rating: { not: null }
-            },
-            data: {
-                response: dto.response,
-                acknowledged_on: new Date(),
+        return this.prisma.$transaction(async (tx) => {
+            try {
+                const acknowledgeEvaluation = await tx.hrEmployeeEvaluation.update({
+                    where: { 
+                        id: evaluationId, 
+                        employee_id: requestUser.employee.id , 
+                        evaluated_on: { not: null }, 
+                        type_of_evaluation: { not: null },
+                        overall_rating: { not: null }
+                    },
+                    data: {
+                        status: "for_verification",
+                        response: dto.response,
+                        acknowledged_on: new Date(),
+                    }
+                })
+
+                if (!acknowledgeEvaluation) {
+                    throw new NotFoundException('No evaluations done yet')
+                }
+
+                await tx.workflowAction.create({
+                    data: {
+                        actionable_type: WORKFLOW_ENTITY.EMPLOYEE_EVALUATION,
+                        actionable_id: evaluationId,
+                        action: "acknowledge",
+                        acted_by: requestUser.id
+                    }
+                })
+
+                const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+                const userPosition = requestUser.employee.position.name;
+
+                return {
+                    status: 'success',
+                    message: 'Evaluation successfully acknowledge',
+                    acknowledgeEvaluation,
+                    acknowledge_by: `${userName} - ${userPosition}`,
+                }
+            } catch (e) {
+                if (e instanceof NotFoundException) {
+                    throw e; // keep your validation errors
+                }
             }
         })
-
-        if (!acknowledgeEvaluation) {
-            throw new NotFoundException('No evaluations done yet')
-        }
-
-        return {
-            status: 'success',
-            message: 'Evaluation successfully acknowledge',
-            acknowledgeEvaluation
-        }
     }
 }
