78429be9cc6c09c990dcdb3b51ff8d9d1e23bb84
erzan12 <do.earljan@gmail.com>
2026-05-13 16:36:29 +0800

    feat: added new workflow constant: extended leave request

--- src/utils/constants/workflow-entity.constants.ts
+++ src/utils/constants/workflow-entity.constants.ts
@@ -1,5 +1,6 @@
 export const WORKFLOW_ENTITY = {
   LEAVE_REQUEST: "LeaveRequest",
+  EXTENDED_LEAVE_REQUEST: "ExtendedLeaveRequest",
   APPLICANT: "Applicant",
   EMPLOYEE_EVALUATION: "EmployeeEvaluation",
   CAREER_POSTING: 'CareerPosting',
