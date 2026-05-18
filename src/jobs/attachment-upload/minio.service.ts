31393fe12a00d952c2ef66680e5d84fa11fb1694
erzan12 <do.earljan@gmail.com>
2026-05-18 13:55:49 +0800

    feat: added minio client handler

---
+++ src/jobs/attachment-upload/minio.service.ts
@@ -0,0 +1,10 @@
+import * as Minio from 'minio';
+
+export const minioClient = new Minio.Client({
+    endPoint: process.env.MINIO_ENDPOINT!,
+    port: Number(process.env.MINIO_PORT),
+    useSSL: process.env.MINIO_USE_SSL === 'true',
+
+    accessKey: process.env.MINIO_ACCESS_KEY!,
+    secretKey: process.env.MINIO_SECRET_KEY!,
+});
