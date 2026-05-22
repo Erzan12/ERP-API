bf7d7b57a9ddac60c0b561046cc9442a37dd20f5
erzan12 <do.earljan@gmail.com>
2026-05-22 14:43:46 +0800

    fix: temporary turn off require pass reset on first log in

--- src/modules/manager/user_management/user_management.service.ts
+++ src/modules/manager/user_management/user_management.service.ts
@@ -349,7 +349,7 @@ export class UserManagementService {
           email: dto.email,
           password: hashedPassword,
           is_active: true,
-          require_reset: 1,
+          require_reset: 0,
           // created_by: ,
           created_at: new Date(),
         },
