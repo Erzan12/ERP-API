2a935b1f4774fc068d856aaaf9dd120a6706723f
erzan12 <do.earljan@gmail.com>
2026-06-01 13:27:37 +0800

    feat: added overtime rate update api controller

--- src/modules/hris/time-and-attendance-cases/overtime-rate/overtime-rate.controller.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-rate/overtime-rate.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { OvertimeRateService } from './overtime-rate.service';
-import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
-import { ACTION_CREATE, ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { ApiGetResponse, ApiPatchResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_CREATE, ACTION_READ, ACTION_UPDATE, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
 import { subject } from '@casl/ability';
 import { Can } from 'src/utils/decorators/can.decorator';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
-import { CreateOvertimeRateDto } from './dto/overtime-rate.dto';
+import { CreateOvertimeRateDto, UpdateOvertimeRateDto } from './dto/overtime-rate.dto';
 
 @ApiTags("Overtime Cases - Overtime Rates")
 @Controller({path: 'hris', version: '2'})
@@ -44,5 +44,21 @@ export class OvertimeRateController {
         @Body() dto: CreateOvertimeRateDto
     ) {
         return this.overtimeService.createOvertimeRate(user, dto);
+    }
+
+    @Put("/time-and-attendance/overtime-rates/:overtimeRateId")
+    @ApiOperation({ summary: 'Update a overtime rate' })
+    @ApiPatchResponse('Overtime rate updated successfully')
+    @Can({ action: ACTION_UPDATE, subject: EMPLOYEE_MASTERLIST })
+    updateOvertimeRate(
+        @SessionUser() user: RequestUser,
+        @Body() dto: UpdateOvertimeRateDto,
+        @Param('overtimeRateId', new ParseUUIDPipe()) overtimeRateId: string
+    ) {
+        return this.overtimeService.updateOvertimeRate(
+            user,
+            dto,
+            overtimeRateId
+        )
     }
 }
