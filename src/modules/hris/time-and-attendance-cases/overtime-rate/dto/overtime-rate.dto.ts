14438b860fd60856ecb1ceed3c3a575605d172f9
erzan12 <do.earljan@gmail.com>
2026-06-01 11:34:17 +0800

    feat: added ot rate dto, added new api get ot rate list

--- src/modules/hris/time-and-attendance-cases/overtime-rate/dto/overtime-rate.dto.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-rate/dto/overtime-rate.dto.ts
@@ -1,3 +1,12 @@
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator";
+
 export class CreateOvertimeRateDto {
-    
+    @IsNotEmpty()
+    @IsString()
+    type: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    rate: number;
 }
--- src/modules/hris/time-and-attendance-cases/overtime-rate/overtime-rate.service.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-rate/overtime-rate.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { RequestUser } from 'src/utils/types/request-user.interface';
 
 @Injectable()
-export class OvertimeRateService {}
+export class OvertimeRateService {
+    constructor(private prisma: PrismaService) {}
+
+    async getOvertimeRates(user: RequestUser) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+    
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const overtimeRates = await this.prisma.hrOvertimeRate.findMany({
+            include: {
+                overtimeRequests: true,
+            }
+        })
+
+        if(overtimeRates.length === 0) {
+            throw new NotFoundException("No overtime rate is available")
+        }
+
+        return {
+            status: 'success',
+            message: 'List of overtime rates available',
+            overtimeRates
+        }
+    }
+}
