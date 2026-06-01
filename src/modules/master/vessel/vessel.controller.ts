6b336c9888a351d1a762be5ef711f211fbf38867
erzan12 <do.earljan@gmail.com>
2026-06-01 09:26:32 +0800

    feat: new vessel scaffolding and crud api for vessel

--- src/modules/master/vessel/vessel.controller.ts
+++ src/modules/master/vessel/vessel.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { VesselService } from './vessel.service';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_CREATE, ACTION_READ, MASTERTABLES } from 'src/utils/constants/ability.constant';
+import { Can } from 'src/utils/decorators/can.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { CreateVesselWithDetailsDto } from './dto/vessel.dto';
 
-@Controller('vessel')
-export class VesselController {}
+@ApiTags('Mastertable - Vessel')
+@Controller({path: 'mastertable', version: '2'})
+export class VesselController {
+    constructor (private readonly vesselService: VesselService) {}
+
+    @Get('vessels/:vesselId')
+    @ApiOperation({ summary: 'Get a single vessel' })
+    @ApiGetResponse('Here is the vessel')
+    @Can({ action: ACTION_READ, subject: MASTERTABLES })
+    getVessel(
+        @SessionUser() user: RequestUser,
+        @Param('vesselId', new ParseUUIDPipe()) vesselId: string,
+    ) {
+        return this.vesselService.getVessel(user, vesselId);
+    }
+
+    @Get('vessels')
+    @ApiOperation({ summary: 'Get all vessels' })
+    @ApiGetResponse('List of vessels retrieved')
+    @Can({ action: ACTION_READ, subject: MASTERTABLES })
+    getVessels(
+        @SessionUser() user: RequestUser
+    ) {
+        return this.vesselService.getVessels(user);
+    }
+
+    @Post('vessels')
+    @ApiBody({ type: CreateVesselWithDetailsDto, description: 'Payload to create vessel' })
+    @ApiOperation({ summary: 'Create a new vessel' })
+    @ApiPostResponse('Vessel created successfully')
+    @Can({ action: ACTION_CREATE, subject: MASTERTABLES })
+    createVessel(
+        @SessionUser() user: RequestUser,
+        @Body() dto: CreateVesselWithDetailsDto,
+    ) {
+        return this.vesselService.createVessel(user, dto)
+    }
+}
