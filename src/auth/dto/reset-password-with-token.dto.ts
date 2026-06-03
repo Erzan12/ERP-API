3d2d8197c9d3dade74f13f72d100ca242488d28b
erzan12 <do.earljan@gmail.com>
2026-06-03 15:00:40 +0800

    fix: move resend email dto to auth module dto

--- src/auth/dto/reset-password-with-token.dto.ts
+++ src/auth/dto/reset-password-with-token.dto.ts
@@ -1,9 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class ResetPasswordWithTokenDto {
   @IsNotEmpty()
   @MinLength(8, { message: 'Password must be atleast 8 characters long' })
   @ApiProperty({ example: 'newpassword', description: 'user new password' })
   newPassword: string;
+}
+
+export class UserEmailResetTokenDto {
+  @IsEmail()
+  @ApiProperty({
+    example: 'employee.email@gmail.com',
+  })
+  @IsNotEmpty()
+  email: string;
 }
