57e2a98ba8ad3f2b025fba43c29b867fdc6e341f
erzan12 <do.earljan@gmail.com>
2026-06-08 13:18:57 +0800

    feat: added new api for overtime, get list OT request with pagination

--- src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.controller.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { OvertimeCasesService } from './overtime-cases.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ACTION_READ, EMPLOYEE_MASTERLIST } from 'src/utils/constants/ability.constant';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { Can } from 'src/utils/decorators/can.decorator';
+import { OvertimeCasesPaginationDto } from 'src/utils/dtos/overtime-cases-pagination.dto';
 
-@Controller('overtime-cases')
-export class OvertimeCasesController {}
+@ApiTags("Human Resources - Time and Attendance Cases (Overtime Request)")
+@Controller({ path:'hris', version: '2' })
+export class OvertimeCasesController {
+
+    constructor(private readonly overtimeCasesService: OvertimeCasesService) {}
+
+    @Get('time-and-attendance-cases/overtimes')
+    @ApiOperation({ summary: 'List of Overtime Requests' })
+    @ApiGetResponse('List of Overtime Requests')
+    @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
+    getOvertimes(
+        @SessionUser() user: RequestUser,
+        @Query() dto: OvertimeCasesPaginationDto
+    ) {
+        return this.overtimeCasesService.getOvertimeCases(user, dto);
+    }
+}
--- src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.service.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.service.ts
@@ -1,4 +1,154 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { WORKFLOW_ENTITY } from 'src/utils/constants/workflow-entity.constants';
+import { OvertimeCasesPaginationDto } from 'src/utils/dtos/overtime-cases-pagination.dto';
+import { RequestUser } from 'src/utils/types/request-user.interface';
 
 @Injectable()
-export class OvertimeCasesService {}
+export class OvertimeCasesService {
+
+    constructor (private readonly prisma: PrismaService) {}
+
+    async getOvertimeCases(user: RequestUser, dto: OvertimeCasesPaginationDto) {
+        const { search, status, sortBy, order, page, perPage } = dto;
+        
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const skip = (page - 1) * perPage;
+
+        const whereCondition: Prisma.HrOvertimeRequestWhereInput = {
+            is_active: true
+        };
+
+        const whereConditions: Prisma.HrOvertimeRequestWhereInput = {};
+
+        if (search) {
+            
+            const terms = search.split(' ');
+
+            whereConditions.OR = terms.flatMap((term) => [
+                {
+                    employee: {
+                        person: {
+                            first_name: { contains: term, mode: 'insensitive' },
+                        },
+                    },
+                },
+                {
+                    employee: {
+                        person: {
+                            last_name: { contains: term, mode: 'insensitive' },
+                        },
+                    },
+                },
+            ]);
+        }
+
+        const allowSortFeilds = [
+            'created_by'
+        ]
+
+        const safeSortBy = allowSortFeilds.includes(sortBy) ? sortBy: 'created_at';
+
+        const [total, overtimes] = await this.prisma.$transaction([
+            this.prisma.hrOvertimeRequest.count({
+                where: {
+                    ...whereCondition,
+                    ...whereConditions
+                }
+            }),
+            this.prisma.hrOvertimeRequest.findMany({
+                where: {
+                    ...whereCondition,
+                    ...whereConditions
+                },
+                skip,
+                take: perPage,
+                orderBy: {
+                    [safeSortBy]: order,
+                },
+            }),
+        ]);
+
+        const overtimeIds = overtimes.map(l => l.id);
+
+        const workflowActions = await this.prisma.workflowAction.findMany({
+            where: {
+                actionable_type: WORKFLOW_ENTITY.OVERTIME_REQUEST,
+                actionable_id: {
+                    in: overtimeIds
+                },
+                action: {
+                    in: ["verification", "approval"]
+                }
+            },
+            include: {
+                acted_by_user: {
+                    select: {
+                        id: true,
+                        employee: {
+                            select: {
+                                person: {
+                                    select: {
+                                        first_name: true,
+                                        middle_name: true,
+                                        last_name: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        const formattedOvertimes = overtimes.map(overtime => {
+            const verifier = workflowActions.find(a =>
+                a.actionable_id === overtime.id &&
+                a.action === "verification"
+            );
+            const approver = workflowActions.find(a =>
+                a.actionable_id === overtime.id &&
+                a.action === "approval"
+            );
+            return {
+                ...overtime,
+                verifier: verifier?.acted_by_user ?? null,
+                approver: approver?.acted_by_user ?? null,
+            }
+        });
+
+        return {
+            status: 'success',
+            message: 'List of Overtime Cases',
+            count: total,
+            page,
+            perPage,
+            overtimes: formattedOvertimes
+        }
+    }
+}
