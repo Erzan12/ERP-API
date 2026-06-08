96e5cb670a80cb6fa810ff0c1942c797b969b5be
erzan12 <do.earljan@gmail.com>
2026-06-08 13:59:43 +0800

    refactor: remove unnecessary white spaces in leave cases module

--- src/modules/hris/time-and-attendance-cases/leave-cases/leave-cases.controller.ts
+++ src/modules/hris/time-and-attendance-cases/leave-cases/leave-cases.controller.ts
@@ -18,7 +18,7 @@ export class LeaveCasesController {
     @ApiOperation({ summary: 'List of all Leave Request' })
     @ApiGetResponse('List of Leave Request')
     @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
-    getLeaves(
+    getLeaveRequest(
         @SessionUser() user: RequestUser,
         @Query() dto: LeaveRequestPaginationDto,
     ) {
@@ -35,15 +35,15 @@ export class LeaveCasesController {
         return this.leaveCasesService.statusCount(user);
     }
 
-    @Get('time-and-attendance-cases/leaves/:hrLeaveRequestId')
+    @Get('time-and-attendance-cases/leaves/:leaveRequestId')
     @ApiOperation({ summary: 'Get a single Leave Request' })
     @ApiGetResponse('Here is the Leave Request')
     @Can({ action: ACTION_READ, subject: EMPLOYEE_MASTERLIST })
-    getLeaveCase(
+    getLeaveRequests(
         @SessionUser() user: RequestUser,
-        @Param("hrLeaveRequestId", new ParseUUIDPipe) hrLeaveRequestId: string,
+        @Param("leaveRequestId", new ParseUUIDPipe) leaveRequestId: string,
     ) {
-        return this.leaveCasesService.getLeaveCase(hrLeaveRequestId, user)
+        return this.leaveCasesService.getLeaveCase(leaveRequestId, user)
     }
 
     @Post('time-and-attendance-cases/leave')
