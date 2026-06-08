d9e32a02f2252bc8646ee65c80e936ca356d9b12
erzan12 <do.earljan@gmail.com>
2026-06-08 13:48:09 +0800

    feat: added api for OT request, get list of OT request

--- src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.service.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { WORKFLOW_ENTITY } from 'src/utils/constants/workflow-entity.constants';
@@ -9,6 +9,134 @@ import { RequestUser } from 'src/utils/types/request-user.interface';
 export class OvertimeCasesService {
 
     constructor (private readonly prisma: PrismaService) {}
+
+    async getOvertimeCase(user: RequestUser, overtimeRequestId: string,) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        try {
+            const overtimeRequest = await this.prisma.hrOvertimeRequest.findUnique({
+                where: { id: overtimeRequestId, is_active: true },
+                include: {
+                    employee: {
+                        select: {
+                            id: true,
+                            employee_id: true,
+                            person: {
+                                select: {
+                                    first_name: true,
+                                    middle_name: true,
+                                    last_name: true
+                                }
+                            },
+                            position: {
+                                select: {
+                                    id: true,
+                                    name: true
+                                }
+                            },
+                            department: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                }
+                            },
+                        },
+                    },
+                    vessel: {
+                        select: {
+                            name: true,
+                            type: true,
+                            company: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            if (!overtimeRequest) {
+                throw new NotFoundException ("Overtime Request not found")
+            }
+
+            const workflowActions = await this.prisma.workflowAction.findMany({
+                where: {
+                    actionable_type: WORKFLOW_ENTITY.OVERTIME_REQUEST,
+                    actionable_id: overtimeRequest.id,
+                    action: {
+                        in: ["verification", "approval"]
+                    }
+                },
+                include: {
+                    acted_by_user: {
+                        select: {
+                            id: true,
+                            employee: {
+                                select: {
+                                    person: {
+                                        select: {
+                                            first_name: true,
+                                            middle_name: true,
+                                            last_name: true
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            const verifier = workflowActions.find(
+                a => a.action === "verification"
+            );
+
+            const approver = workflowActions.find(
+                a => a.action === "approval"
+            )
+
+            const overtime = {
+                ...overtimeRequest,
+                verifier: verifier?.acted_by_user ?? null,
+                approver: approver?.acted_by_user ?? null,
+            };
+
+            return {
+                status: 'success',
+                message: 'Here is the Overtime Request',
+                overtimeRequest: overtime
+            }
+        } catch (e) {
+            if (e instanceof NotFoundException) {
+                throw e;
+            }
+        }
+    }
 
     async getOvertimeCases(user: RequestUser, dto: OvertimeCasesPaginationDto) {
         const { search, status, sortBy, order, page, perPage } = dto;
