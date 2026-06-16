934e6660afaedda47d505240bca227d6f1899725
erzan12 <do.earljan@gmail.com>
2026-06-16 15:28:27 +0800

    config: updated api version for admin and hris module

--- src/app/hris/hris.swagger.ts
+++ src/app/hris/hris.swagger.ts
@@ -38,7 +38,7 @@ export function setupHRISSwagger(app: INestApplication): void {
     .setDescription(
       'API for HRIS employee lifecycle. CURRENTLY VIEWING API VERSION 2',
     )
-    .setVersion('2.1')
+    .setVersion('2.2')
     .addTag('Authentication')
     .addTag('Human Resources - Dashboard')
     .build();
