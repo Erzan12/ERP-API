b36ebaeb07f279fefad833a014cb93d41f269f0d
erzan12 <do.earljan@gmail.com>
2026-06-16 15:21:24 +0800

    refactored: module pagination dto to one dto
    for both sub module and module pagination

--- src/utils/dtos/module-pagination.dto.ts
+++ src/utils/dtos/module-pagination.dto.ts
@@ -2,6 +2,40 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional, IsString } from 'class-validator';
 
+export class ModulePaginationDto {
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ default: '' })
+  search?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ default: '' })
+  sub_module_id?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ example: 'created_at', default: 'created_at' })
+  sortBy: string = 'created_at';
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({ example: 'desc', default: 'desc' })
+  order: 'asc' | 'desc' = 'asc';
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @ApiPropertyOptional({ example: 1, default: 1 })
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @ApiPropertyOptional({ example: 10, default: 10 })
+  perPage: number = 10;
+}
+
 export class SubModulePaginationDto {
   @IsOptional()
   @IsString()
@@ -11,7 +45,7 @@ export class SubModulePaginationDto {
   @IsOptional()
   @IsString()
   @ApiPropertyOptional({ default: '' })
-  module?: string;
+  module_id?: string;
 
   @IsOptional()
   @IsString()
