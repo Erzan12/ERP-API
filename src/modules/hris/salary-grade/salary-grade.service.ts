12077fcba56b9a536f88b85a67445cbba50f1d2d
erzan12 <do.earljan@gmail.com>
2026-06-16 10:43:58 +0800

    feat: added new api create salary grade

--- src/modules/hris/salary-grade/salary-grade.service.ts
+++ src/modules/hris/salary-grade/salary-grade.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateSalaryGradeDto } from './dto/salary-grade.dto';
 
 @Injectable()
 export class SalaryGradeService {
@@ -45,6 +46,63 @@ export class SalaryGradeService {
             status: 'success',
             message: 'List of Salary Grades',
             salaryGrades
+        }
+    }
+
+    async createSalaryGrade(user: RequestUser, dto: CreateSalaryGradeDto) {
+        const { grade, rate, level, is_confidential } = dto;
+
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const existingGrade = await this.prisma.salaryGrade.findFirst({
+            where: { grade: dto.grade }
+        })
+
+        if (existingGrade) {
+            throw new ConflictException("Salary Grade already exist!");
+        }
+
+        const salaryGrade = await this.prisma.salaryGrade.create({
+            data: {
+                grade,
+                rate,
+                level,
+                is_confidential,
+                created_by: user.id
+            }
+        })
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPosition = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: 'Salary Grade added successfully',
+            salaryGrade,
+            created_by: `${userName} - ${userPosition}`
         }
     }
 }
