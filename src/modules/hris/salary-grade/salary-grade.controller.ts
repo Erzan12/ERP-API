5310b6309b29d90565891519551063163fad84ed
erzan12 <do.earljan@gmail.com>
2026-06-16 10:43:58 +0800

    feat: added new api create salary grade

--- src/modules/hris/salary-grade/salary-grade.controller.ts
+++ src/modules/hris/salary-grade/salary-grade.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SalaryGradeService } from './salary-grade.service';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
-import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
+import { CreateSalaryGradeDto } from './dto/salary-grade.dto';
 
 @ApiTags('Human Resources - Salary Grade')
 @Controller({path:'hris', version: '2'})
@@ -12,11 +13,25 @@ export class SalaryGradeController {
     constructor (private readonly salaryGradeService: SalaryGradeService) {}
 
     @Get('salary-grades')
-    @ApiOperation({ summary: 'List of all Salary Grades'})
+    @ApiOperation({ summary: 'List of all Salary Grades' })
     @ApiGetResponse('List of Salary Grades')
     getSalaryGrades(
         @SessionUser() user: RequestUser
     ) {
         return this.salaryGradeService.getSalaryGrades(user);
+    }
+
+    @Post('salary-grades')
+    @ApiBody({
+        type: CreateSalaryGradeDto,
+        description: 'Payload to create Salary Grade'
+    })
+    @ApiOperation({ summary: 'Create a Salary Grade' })
+    @ApiPostResponse('Salary Grade successfully created')
+    createSalaryGrade(
+        @Body() dto: CreateSalaryGradeDto,
+        @SessionUser() user: RequestUser
+    ) {
+        return this.salaryGradeService.createSalaryGrade(user, dto);
     }
 }
