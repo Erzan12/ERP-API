6d0f706500de54498a494edc250f41e7efab4bf0
erzan12 <do.earljan@gmail.com>
2026-06-18 10:42:49 +0800

    feat: finalized submodule action CRUD api

---
+++ src/modules/administrator/sub-module-action/sub-module-action.service.ts
@@ -0,0 +1,188 @@
+import { BadRequestException, ConflictException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { RequestUser } from 'src/utils/types/request-user.interface';
+import { CreateSubModuleActionDto, UpdateSubmoduleActionDto } from './dto/sub-module-action.dto';
+
+@Injectable()
+export class SubModuleActionService {
+
+    constructor (private readonly prisma: PrismaService) {}
+
+    async getSubModuleActions(user: RequestUser) {
+        const modules = await this.prisma.subModuleAction.findMany();
+
+        if (modules.length === 0) {
+            throw new NotFoundException(
+            'No Submodule actions yet available or added',
+            );
+        }
+
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+            employee: {
+                include: {
+                person: true,
+                position: true,
+                },
+            },
+            user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const isAdmin = requestUser.user_roles.some(
+            (role) =>
+            // role.role_id === 'b1118e05-6377-4e64-a677-14f9b9226fdd' &&
+            role.role_name === 'Administrator' || 'Super Administrator',
+        );
+
+        if (!isAdmin) {
+            throw new ForbiddenException(
+            'You are not allowed to perform this action',
+            );
+        }
+
+        return {
+            status: 'success',
+            message: 'Here is the list of Submodule Actions available',
+            modules,
+        };
+    }
+
+    //add new submodule permission -> acts as inventory of all permisison/actions that can be assigned to a submodule
+    async createSubModuleAction(dto: CreateSubModuleActionDto, user: RequestUser) {
+        const { action } = dto;
+
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const existingAction = await this.prisma.subModuleAction.findFirst({
+            where: { action }
+        })
+
+        if (existingAction) {
+            throw new ConflictException("Sub Module Action already exist!")
+        }
+
+        const subModuleAction = await this.prisma.subModuleAction.create({
+            data: {
+                action,
+                created_by: user.id
+            }
+        });
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPos = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: `Added new Sub Module action successfully`,
+            created_by: `${userName} - ${userPos}`,
+            subModuleAction
+        };
+    }
+
+    async updateSubmoduleAction(
+        dto: UpdateSubmoduleActionDto,
+        user: RequestUser,
+        subModuleActionId: string,
+    ) {
+        const { action, is_active } = dto;
+
+        const existingSubModulePermission =
+            await this.prisma.subModuleAction.findFirst({
+            where: { id: subModuleActionId },
+            });
+
+        if (!existingSubModulePermission) {
+            throw new NotFoundException('Sub Module permission does not exist!');
+        }
+
+        // if (existingSubModulePermission.stat === 0) {
+        //     throw new ForbiddenException(`${existingSubModulePermission.action} action status is inactive`);
+        // }
+
+        const updateSubModulePermission = await this.prisma.subModuleAction.update({
+            where: { id: subModuleActionId },
+            data: {
+            id: existingSubModulePermission.id,
+            action,
+            is_active,
+            },
+        });
+
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+            employee: {
+                include: {
+                person: true,
+                position: true,
+                },
+            },
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+
+        const userName = `${requestUser.employee.person.first_name} ${requestUser.employee.person.last_name}`;
+        const userPos = requestUser.employee.position.name;
+
+        return {
+            status: 'success',
+            message: `${existingSubModulePermission.action} action has been updated successfully!`,
+            updated_by: {
+            id: requestUser.id,
+            name: userName,
+            position: userPos,
+            },
+            data: {
+            updateSubModulePermission,
+            },
+        };
+    }
+
+    async deleteSubmoduleAction(submoduleActionId: string) {
+        const submoduleAction = await this.prisma.subModuleAction.delete({
+            where: { id: submoduleActionId },
+        })
+
+        if (!submoduleAction) {
+            throw new NotFoundException ("Submodule Action does not exist");
+        }
+
+        return {
+            status: 'success',
+            message: 'Sub module action deleted successfully',
+            submoduleAction
+        }
+    }
+}
