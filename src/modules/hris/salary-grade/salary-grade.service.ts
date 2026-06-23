4a1e3e93e31bf37e98431fca6e1607f729696b8f
erzan12 <do.earljan@gmail.com>
2026-06-23 13:09:43 +0800

    feat: added get single id salary grade api

--- src/modules/hris/salary-grade/salary-grade.service.ts
+++ src/modules/hris/salary-grade/salary-grade.service.ts
@@ -8,6 +8,47 @@ export class SalaryGradeService {
 
     constructor (private readonly prisma:PrismaService) {}
 
+    async getSalaryGrade(salaryGradeId: string, user: RequestUser) {
+        // Auth check first
+        const requestUser = await this.prisma.user.findUnique({
+            where: { id: user.id },
+            include: {
+                employee: {
+                include: {
+                    person: true,
+                    position: true,
+                },
+                },
+                user_roles: true,
+            },
+        });
+
+        if (!requestUser || !requestUser.employee || !requestUser.employee.person) {
+            throw new BadRequestException(`User does not exist.`);
+        }
+    
+        const allowedRoles = ['Administrator', 'Super Administrator', 'HR Manager', 'HR Clerk', 'HR Staff'];
+        const canView = requestUser?.user_roles.some(role => allowedRoles.includes(role.role_name));
+    
+        if (!canView) {
+            throw new ForbiddenException('You are not authorized to perform this action');
+        }
+
+        const salaryGrade = await this.prisma.salaryGrade.findUnique({
+            where: { id: salaryGradeId, is_active: true},
+        });
+
+        if (!salaryGrade) {
+            throw new NotFoundException("Salary Grade does not exist!");
+        }
+
+        return {
+            status: 'success',
+            message: 'Here is the Salary Grade',
+            salaryGrade
+        }
+    }
+
     async getSalaryGrades(user: RequestUser) {
         // Auth check first
         const requestUser = await this.prisma.user.findUnique({
