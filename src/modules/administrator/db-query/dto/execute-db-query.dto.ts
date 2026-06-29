03ab862fdbca0169ff6b98a09978ad5694b0a441
erzan12 <do.earljan@gmail.com>
2026-06-29 11:42:29 +0800

    refactor: db query in adminstrator module added validator and params

--- src/modules/administrator/db-query/dto/execute-db-query.dto.ts
+++ src/modules/administrator/db-query/dto/execute-db-query.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ExecuteDbQueryDto {
