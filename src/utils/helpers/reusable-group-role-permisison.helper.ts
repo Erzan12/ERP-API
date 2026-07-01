a93196e9f9a9009b1b38d738dfe8cdf140e0083c
erzan12 <do.earljan@gmail.com>
2026-07-01 16:36:54 +0800

    feat: updated reusable roles mapper to include submodule permission

--- src/utils/helpers/reusable-group-role-permisison.helper.ts
+++ src/utils/helpers/reusable-group-role-permisison.helper.ts
@@ -3,16 +3,6 @@ import { RequestUser } from '../types/request-user.interface';
 
 export function mapRolesToRequestUser(
   userRoles: Array<{
-    // role: {
-    //   id: string;
-    //   name: string;
-    //   role_permissions: Array<{
-    //     action: string,
-    //     sub_module_permission: {
-    //       sub_module: { id: string; name: string };
-    //     }
-    //   }>;
-    // };
     id: string;
     role_name: string;
     user_permissions: Array<{
@@ -24,7 +14,14 @@ export function mapRolesToRequestUser(
             name: string;
           };
         };
-      };
+      } | null;
+
+      sub_module_permission: {
+        sub_module: {
+          id: string;
+          name: string;
+        };
+      } | null;
     }>;
   }>,
 ): RequestUser['roles'] {
@@ -36,18 +33,28 @@ export function mapRolesToRequestUser(
     >();
 
     for (const up of ur.user_permissions) {
-      const subModule = up.role_permission.sub_module_permission.sub_module;
+      // const subModule = up.role_permission.sub_module_permission.sub_module;
+      const subModule =
+        up.role_permission?.sub_module_permission?.sub_module ??
+        up.sub_module_permission?.sub_module;
+
+      if (!subModule) {
+        continue;
+      }
 
       const key = subModule.id;
 
       if (!subModuleMap.has(key)) {
         subModuleMap.set(key, {
-          id: up.role_permission.sub_module_permission.sub_module.id,
-          name: up.role_permission.sub_module_permission.sub_module.name,
+          id: subModule.id,
+          name: subModule.name,
           actions: [],
         });
       }
-      subModuleMap.get(key)!.actions.push(up.action.toLowerCase().trim());
+
+      subModuleMap.get(key)!.actions.push(
+        up.action.toLowerCase().trim(),
+      );
     }
 
     return {
