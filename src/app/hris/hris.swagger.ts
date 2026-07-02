f18d51581c281252fa2057e8c5ab31a44c43081f
erzan12 <do.earljan@gmail.com>
2026-07-02 09:31:57 +0800

    feat: updated swagger api version for current modules
    since all modules right now utilizing there subject in there controller @Can decorator

--- src/app/hris/hris.swagger.ts
+++ src/app/hris/hris.swagger.ts
@@ -38,7 +38,7 @@ export function setupHRISSwagger(app: INestApplication): void {
     .setDescription(
       'API for HRIS employee lifecycle. CURRENTLY VIEWING API VERSION 2',
     )
-    .setVersion('2.4')
+    .setVersion('2.4.1')
     .addTag('Authentication')
     .addTag('Human Resources - Dashboard')
     .build();
