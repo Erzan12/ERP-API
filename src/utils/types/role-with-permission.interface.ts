0b2c60b16907b6b59cb2287c3510e3b5d1e36827
erzan12 <do.earljan@gmail.com>
2026-07-03 16:45:33 +0800

    feat: added new api for role management

--- src/utils/types/role-with-permission.interface.ts
+++ src/utils/types/role-with-permission.interface.ts
@@ -4,6 +4,7 @@ export type RoleWithPermissions = Prisma.RoleGetPayload<{
   include: {
     role_permissions: {
       select: {
+        id: true,
         role: {
           select: {
             id: true;
@@ -16,6 +17,23 @@ export type RoleWithPermissions = Prisma.RoleGetPayload<{
             };
           };
         };
+        // sub_module_permission: {
+        //   select: {
+        //     id: true;
+        //     actions: {
+        //       role_permission_id: string;
+        //       sub_module_permission_id: string;
+        //       action: string;
+        //     }[];
+        //     sub_module_action_id: true;
+        //     sub_module: {
+        //       select: {
+        //         id: true;
+        //         name: true;
+        //       };
+        //     };
+        //   };
+        // };
         sub_module_permission: {
           select: {
             id: true;
@@ -28,8 +46,58 @@ export type RoleWithPermissions = Prisma.RoleGetPayload<{
               };
             };
           };
+        }
+      };
+    };
+  };
+}>;
+
+export type UserRolePermissions = Prisma.RoleGetPayload<{
+  select: {
+    id: true,
+    sub_module_permission_id: true,
+    is_active: true,
+    // sub_module_permission: {
+    //   select: {
+    //     id: true;
+    //     actions: {
+    //       role_permission_id: string;
+    //       sub_module_permission_id: string;
+    //       action: string;
+    //     }[];
+    //     sub_module_action_id: true;
+    //     sub_module: {
+    //       select: {
+    //         id: true;
+    //         name: true;
+    //       };
+    //     };
+    //   };
+    // };
+    role: {
+      select: {
+        id: true;
+        name: true;
+        department: {
+          select: {
+            id: true;
+            name: true;
+          };
         };
       };
     };
+    sub_module_permission: {
+      select: {
+        id: true;
+        action: true;
+        sub_module_action_id: true;
+        sub_module: {
+          select: {
+            id: true;
+            name: true;
+          };
+        };
+      };
+    }
   };
 }>;
