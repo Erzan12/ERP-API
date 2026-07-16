0104d4481ddd4b6a48eb0c05764654cc3075455d
erzan12 <do.earljan@gmail.com>
2026-07-16 10:46:33 +0800

    feat: updated extended leave request create api add conflict validator

--- src/modules/hris/time-and-attendance-cases/extended-leave-cases/extended-leave-cases.service.ts
+++ src/modules/hris/time-and-attendance-cases/extended-leave-cases/extended-leave-cases.service.ts
@@ -6,7 +6,10 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma/prisma.service';
 import { RequestUser } from 'src/utils/types/request-user.interface';
-import { CreateExtendedLeaveRequestWithDetailsDto, UpdateExtendedLeaveRequestWithDetailsDto } from './dto/extended-leave-request.dto';
+import {
+  CreateExtendedLeaveRequestWithDetailsDto,
+  UpdateExtendedLeaveRequestWithDetailsDto,
+} from './dto/extended-leave-request.dto';
 import { WORKFLOW_ENTITY } from 'src/utils/constants/workflow-entity.constants';
 import { LeaveRequestStatus, Prisma, WorkflowActionType } from '@prisma/client';
 import { LeaveRequestPaginationDto } from 'src/utils/dtos/leave-request-pagination.dto';
@@ -340,6 +343,73 @@ export class ExtendedLeaveCasesService {
         );
       }
 
+      if (extended_leave_dates.length === 0) {
+        throw new BadRequestException('Extended leave dates cannot be empty');
+      }
+
+      const from = new Date(extended_leave_request.extension_date_from);
+      const to = new Date(extended_leave_request.extension_date_to);
+
+      const expectedDates: string[] = [];
+
+      const current = new Date(from);
+      while (current <= to) {
+        expectedDates.push(current.toISOString().split('T')[0]);
+        current.setDate(current.getDate() + 1);
+      }
+
+      const inputDates = extended_leave_dates.map(
+        (d) => new Date(d.leave_date).toISOString().split('T')[0],
+      );
+
+      if (expectedDates.length !== inputDates.length) {
+        throw new BadRequestException(
+          'Extended leave dates do not match the selected date range'
+        )
+      }
+
+      const missingDates = expectedDates.filter((d) => !inputDates.includes(d));
+
+      if (missingDates.length > 0) {
+        throw new BadRequestException(
+          `Missing extended leave dates: ${missingDates.join(', ')}`,
+        );
+      }
+
+      const uniqueDates = new Set(extended_leave_dates.map((d) => d.leave_date));
+
+      if (uniqueDates.size !== extended_leave_dates.length) {
+        throw new BadRequestException('Duplicate extended leaave dates detected');
+      }
+
+      const no_of_days = extended_leave_dates.reduce(
+        (sum, d) => sum + (d.fraction ?? 1),
+        0,
+      );
+
+      const conflict = await tx.hrLeaveDates.findFirst({
+        where: {
+          employee_id: leaveRequest.employee_id,
+          leave_date: {
+            in: extended_leave_dates.map((d) => new Date(d.leave_date)),
+          },
+          hr_extended_leave_request: {
+            extended_leave_request_status: {
+              notIn: [
+                LeaveRequestStatus.cancelled,
+                LeaveRequestStatus.rejected,
+              ],
+            },
+          },
+        },
+      });
+
+      if (conflict) {
+        throw new BadRequestException(
+          'Conflicting leave date exists (active request already exist)',
+        );
+      }
+
       const extendedLeaveRequest =
         await this.prisma.hrExtendedLeaveRequest.create({
           data: {
@@ -359,6 +429,7 @@ export class ExtendedLeaveCasesService {
             reason_for_extension: extended_leave_request.reason_for_extension,
             contact_no_while_on_leave: extended_leave_request.contact_number,
             address_while_on_leave: extended_leave_request.address_on_leave,
+            no_of_days: no_of_days,
             hr_leave_dates: {
               create: extended_leave_dates.map((d) => ({
                 leave_date: new Date(d.leave_date),
@@ -532,13 +603,13 @@ export class ExtendedLeaveCasesService {
   }
 
   async updateExtendedLeaveRequest(
-    user: RequestUser, 
-    dto: UpdateExtendedLeaveRequestWithDetailsDto, 
-    extendedHrLeaveRequestId: string
+    user: RequestUser,
+    dto: UpdateExtendedLeaveRequestWithDetailsDto,
+    extendedHrLeaveRequestId: string,
   ) {
     const { update_extended_leave_request, update_extended_leave_dates } = dto;
 
-     // Auth check first
+    // Auth check first
     const requestUser = await this.prisma.user.findUnique({
       where: { id: user.id },
       include: {
@@ -575,16 +646,17 @@ export class ExtendedLeaveCasesService {
     }
 
     return this.prisma.$transaction(async (tx) => {
-      const existingExtendedLeaveRequest = await tx.hrExtendedLeaveRequest.findUnique({
-        where: {
-          id: extendedHrLeaveRequestId,
-          is_active: true,
-          extended_leave_request_status: LeaveRequestStatus.draft,
-        },
-        include: {
-          hr_leave_request: true
-        }
-      });
+      const existingExtendedLeaveRequest =
+        await tx.hrExtendedLeaveRequest.findUnique({
+          where: {
+            id: extendedHrLeaveRequestId,
+            is_active: true,
+            extended_leave_request_status: LeaveRequestStatus.draft,
+          },
+          include: {
+            hr_leave_request: true,
+          },
+        });
 
       if (!existingExtendedLeaveRequest) {
         throw new NotFoundException(
@@ -593,7 +665,10 @@ export class ExtendedLeaveCasesService {
       }
 
       // Process your extended leave dates list
-      if (update_extended_leave_dates && update_extended_leave_dates.length > 0) {
+      if (
+        update_extended_leave_dates &&
+        update_extended_leave_dates.length > 0
+      ) {
         for (const d of update_extended_leave_dates) {
           if (d.id) {
             // Update an existing row using its specific record id
@@ -607,15 +682,17 @@ export class ExtendedLeaveCasesService {
             }
             if (!d.extended_leave_compensation) {
               throw new BadRequestException(
-                'extended_leave_compensation is required for new date entries'
+                'extended_leave_compensation is required for new date entries',
               );
             }
-            
+
             await tx.hrLeaveDates.create({
               data: {
-                hr_leave_request_id: existingExtendedLeaveRequest.hr_leave_request.id,
+                hr_leave_request_id:
+                  existingExtendedLeaveRequest.hr_leave_request.id,
                 hr_extended_leave_request_id: extendedHrLeaveRequestId,
-                employee_id: existingExtendedLeaveRequest.hr_leave_request.employee_id,
+                employee_id:
+                  existingExtendedLeaveRequest.hr_leave_request.employee_id,
                 leave_date: new Date(d.extended_leave_date),
                 leave_compensation: d.extended_leave_compensation,
                 fraction: d.fraction ?? 1.0,
@@ -738,11 +815,12 @@ export class ExtendedLeaveCasesService {
         const approverName = targetUser
           ? `${targetUser.employee.person.first_name} ${targetUser.employee.person.last_name}`.trim()
           : 'Unknown User';
-          
+
         if (currentApprovalStep) {
           // If assigned approver change, update the row
           if (
-            currentApprovalStep.acted_by !== update_extended_leave_request.approver_id
+            currentApprovalStep.acted_by !==
+            update_extended_leave_request.approver_id
           ) {
             await tx.workflowAction.update({
               where: { id: currentApprovalStep.id },
@@ -778,25 +856,31 @@ export class ExtendedLeaveCasesService {
       }
 
       // Update the parent master Extended Leave Request (IDs Remove from here)
-      const updatedExtendedLeaveRequest = await tx.hrExtendedLeaveRequest.update({
-        where: { id: extendedHrLeaveRequestId },
-        data: {
-          extension_date_from: update_extended_leave_request.extension_date_from
-            ? new Date(update_extended_leave_request.extension_date_from) 
-            : undefined,
-          extension_date_to: update_extended_leave_request.extension_date_to
-            ? new Date(update_extended_leave_request.extension_date_to)
-            : undefined,
-          return_date: update_extended_leave_request.return_date
-            ? new Date(update_extended_leave_request.return_date)
-            : undefined,
-          reason_for_extension: update_extended_leave_request.reason_for_extension ?? undefined,
-          contact_no_while_on_leave: update_extended_leave_request.contact_number ?? undefined,
-          address_while_on_leave: update_extended_leave_request.address_on_leave ?? undefined,
-          reliever_id: update_extended_leave_request.reliever_id ?? undefined,
-          // Note: verifier_id and approver_id are omitted completely because they don't exist here!
-        }
-      })
+      const updatedExtendedLeaveRequest =
+        await tx.hrExtendedLeaveRequest.update({
+          where: { id: extendedHrLeaveRequestId },
+          data: {
+            extension_date_from:
+              update_extended_leave_request.extension_date_from
+                ? new Date(update_extended_leave_request.extension_date_from)
+                : undefined,
+            extension_date_to: update_extended_leave_request.extension_date_to
+              ? new Date(update_extended_leave_request.extension_date_to)
+              : undefined,
+            return_date: update_extended_leave_request.return_date
+              ? new Date(update_extended_leave_request.return_date)
+              : undefined,
+            reason_for_extension:
+              update_extended_leave_request.reason_for_extension ?? undefined,
+            contact_no_while_on_leave:
+              update_extended_leave_request.contact_number ?? undefined,
+            address_while_on_leave:
+              update_extended_leave_request.address_on_leave ?? undefined,
+            no_of_days: total_no_of_days,
+            reliever_id: update_extended_leave_request.reliever_id ?? undefined,
+            // Note: verifier_id and approver_id are omitted completely because they don't exist here!
+          },
+        });
 
       // Optional Log the "update" action itself for history trail
       await tx.workflowAction.create({
@@ -819,8 +903,8 @@ export class ExtendedLeaveCasesService {
         status: 'success',
         message: 'Extended Leave Request updated successfully',
         updatedExtendedLeaveRequest,
-      }
-    })
+      };
+    });
   }
 
   async statusCount(user: RequestUser) {
