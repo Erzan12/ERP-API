b00b9e84514d32cdfb7bcdf22f3d0bd6c3af15c6
erzan12 <do.earljan@gmail.com>
2026-07-21 15:43:18 +0800

    feat: added create overtime case api with helpers to calculate date
    and time and get employment history of the employee

--- src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.controller.ts
+++ src/modules/hris/time-and-attendance-cases/overtime-cases/overtime-cases.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Param, ParseUUIDPipe, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Query } from '@nestjs/common';
 import { OvertimeCasesService } from './overtime-cases.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ApiGetResponse } from 'src/utils/helpers/swagger-response.helper';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiGetResponse, ApiPostResponse } from 'src/utils/helpers/swagger-response.helper';
 import {
+  ACTION_CREATE,
   ACTION_READ,
-  OVERTIME_REQUEST
+  OVERTIME_REQUEST,
 } from 'src/utils/constants/ability.constant';
 import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 import { Can } from 'src/utils/decorators/can.decorator';
 import { OvertimeCasesPaginationDto } from 'src/utils/dtos/overtime-cases-pagination.dto';
+import { CreateOvertimeCaseDto } from './dto/overtime-case.dto';
 
 @ApiTags('Human Resources - Time and Attendance Cases (Overtime Request)')
 @Controller({ path: 'hris', version: '2' })
@@ -36,5 +38,20 @@ export class OvertimeCasesController {
     @Param('overtimeRequestId', new ParseUUIDPipe()) overtimeRequestId: string,
   ) {
     return this.overtimeCasesService.getOvertimeCase(user, overtimeRequestId);
+  }
+
+  @Post('time-and-attendance-cases/overtimes')
+  @ApiBody({
+    type: CreateOvertimeCaseDto,
+    description: 'Payload to create Overtime Request',
+  })
+  @ApiOperation({ summary: 'Create a overtime request' })
+  @ApiPostResponse('Overtime request successfully created')
+  @Can({ action: ACTION_CREATE, subject: OVERTIME_REQUEST })
+  createOvertimeRequest(
+    @SessionUser() user: RequestUser,
+    @Body() dto: CreateOvertimeCaseDto,
+  ) {
+    return this.overtimeCasesService.createOvertimeCase(user, dto);
   }
 }
