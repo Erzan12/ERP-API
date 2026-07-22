5fa16c54f51aec2b3ad36653c7523f7fa283df0b
erzan12 <do.earljan@gmail.com>
2026-07-22 13:12:47 +0800

    fix: added fix to create submodule api where slug will not be created

--- src/modules/administrator/role/role.service.ts
+++ src/modules/administrator/role/role.service.ts
@@ -24,6 +24,10 @@ export class RoleService {
   private formatRolePermissions(role: RoleWithPermissions) {
     const { role_permissions, ...rest } = role;
 
+    // console.dir(role.role_permissions[0].sub_module_permission, {
+    //   depth: null,
+    // });
+
     const groupedPermissions = role_permissions.reduce<
       Record<string, GroupedPermission>
     >((acc, permission) => {
@@ -45,6 +49,7 @@ export class RoleService {
       acc[subModuleId].actions.push({
         role_permission_id: permission.id,
         sub_module_permission_id: permission.sub_module_permission.id,
+        code: permission.sub_module_permission.code ?? '',
         action: permission.sub_module_permission.action,
       });
 
@@ -173,6 +178,7 @@ export class RoleService {
                   id: true,
                   sub_module_action_id: true,
                   action: true,
+                  code: true,
                   sub_module: {
                     select: {
                       id: true,
@@ -1031,6 +1037,7 @@ export class RolePermissionService {
       Record<string, GroupedPermission>
     >((acc, permission) => {
       const subModule = permission.sub_module_permission.sub_module;
+      const subModuleCode = permission.sub_module_permission ?? undefined;
       const subModuleId = subModule.id;
 
       if (!acc[subModuleId]) {
@@ -1048,6 +1055,7 @@ export class RolePermissionService {
       acc[subModuleId].actions.push({
         role_permission_id: permission.id,
         sub_module_permission_id: permission.sub_module_permission.id,
+        code: permission.sub_module_permission.code ?? '',
         action: permission.sub_module_permission.action,
       });
 
