697034f6ea9b9aad2e098b24f311797472e84e60
erzan12 <do.earljan@gmail.com>
2026-07-30 11:50:46 +0800

    feat: updated bulk assign interviewer dto

--- src/modules/hris/recruitment-and-onboarding/hiring-pipeline/dto/bulk-assign-interviewer.dto.ts
+++ src/modules/hris/recruitment-and-onboarding/hiring-pipeline/dto/bulk-assign-interviewer.dto.ts
@@ -8,7 +8,10 @@ import {
   IsDateString,
   IsString,
   IsOptional,
+  ValidateNested,
 } from 'class-validator';
+import { InterviewAssignmentDto } from './interview-assignment.dto';
+import { Type } from 'class-transformer';
 
 export class BulkAssignInterviewDto {
   @IsUUID()
@@ -20,22 +23,41 @@ export class BulkAssignInterviewDto {
   applicant_id: string;
 
   @IsArray()
-  @IsUUID('4', { each: true })
-  @ArrayMinSize(3)
-  @ArrayMaxSize(3)
-  @ApiProperty({ example: ['uuid-1', 'uuid-2', 'uuid-3'] })
-  interviewer_ids: string[]; //order: Initial -> Second -> Final
-
-  @IsDateString()
-  @IsNotEmpty()
+  @ArrayMinSize(1)
+  // @ArrayMaxSize(3)
+  @ValidateNested({ each: true })
   @ApiProperty({
-    example: '',
-    description: 'The date of interview of the applicant',
+    description: 'List of interview assignments for the applicant.',
+    type: [InterviewAssignmentDto],
+    example: [
+      {
+        employee_id: 'EMP-UUID',
+        stage: 'initial',
+        date_of_interview: '2026-08-01T09:00:00.000Z',
+        remarks: 'Initial HR screening.',
+      },
+      {
+        employee_id: 'EMP-UUID',
+        stage: 'second',
+        date_of_interview: '2026-08-03T10:00:00.000Z',
+        remarks: 'Technical interview.',
+      },
+      {
+        employee_id: 'EMP-UUID',
+        stage: 'final',
+        date_of_interview: '2026-08-05T01:00:00.000Z',
+        remarks: 'Final interview with the department head.',
+      },
+    ],
   })
-  date_of_interview: string;
+  @Type(() => InterviewAssignmentDto)
+  interviews: InterviewAssignmentDto[]; //order: Initial -> Second -> Final
 
-  @IsString()
-  @IsOptional() // remark for the interviewer like a note why he/she will be helpfull for this interview e.g experience
-  @ApiProperty({ example: '', description: 'The interviewees personal remark' })
-  remarks: string = '';
+  // @IsDateString()
+  // @IsNotEmpty()
+  // @ApiProperty({
+  //   example: '',
+  //   description: 'The date of interview of the applicant',
+  // })
+  // date_of_interview: string;
 }
