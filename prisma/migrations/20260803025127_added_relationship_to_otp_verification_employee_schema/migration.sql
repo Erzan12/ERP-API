d6501b0c19ac8fbf5a24ac272ff5505bdb9ec28e
erzan12 <do.earljan@gmail.com>
2026-08-04 13:33:37 +0800

    feat: new migration for sms notification and otp verification

---
+++ prisma/migrations/20260803025127_added_relationship_to_otp_verification_employee_schema/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - Added the required column `employee_id` to the `OtpVerification` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "OtpPurposeTemplate" AS ENUM ('forgot_password', 'login', 'verify_phone', 'register_employee');
+
+-- AlterTable
+ALTER TABLE "OtpVerification" ADD COLUMN     "employee_id" UUID NOT NULL,
+ALTER COLUMN "user_id" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "OtpVerification" ADD CONSTRAINT "OtpVerification_employee_id_fkey" FOREIGN KEY ("employee_id") REFERENCES "Employee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
