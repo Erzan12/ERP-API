4a75d5cb5a82f03567b6fe157588082546f659cc
erzan12 <do.earljan@gmail.com>
2026-08-04 15:09:05 +0800

    patch: fix merge runner error

--- prisma/migrations/20260801034531_added_new_enum_type_sms_template/migration.sql
+++ prisma/migrations/20260801034531_added_new_enum_type_sms_template/migration.sql
@@ -1,2 +1,19 @@
 -- CreateEnum
-CREATE TYPE "SmsTemplate" AS ENUM ('welcome', 'otp', 'leave_approved', 'leave_rejected', 'payslip_ready', 'regularization', 'overtime_approved');
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_type
+        WHERE typname = 'OtpPurposeTemplate'
+    ) THEN
+        CREATE TYPE "SmsTemplate" AS ENUM (
+            'welcome', 
+            'otp', 
+            'leave_approved', 
+            'leave_rejected', 
+            'payslip_ready', 
+            'regularization', 
+            'overtime_approved'
+        );
+    END IF;
+END $$;
