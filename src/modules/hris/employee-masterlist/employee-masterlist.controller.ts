910eea1e55df0d8fe82f95a32f43daf7882a5cd9
erzan12 <do.earljan@gmail.com>
2026-08-04 13:24:11 +0800

    feat: added new api endpoint in employee masterlist controller
    for sms notification registration and verify otp

--- src/modules/hris/employee-masterlist/employee-masterlist.controller.ts
+++ src/modules/hris/employee-masterlist/employee-masterlist.controller.ts
@@ -36,6 +36,8 @@ import { SessionUser } from 'src/utils/decorators/session-user.decorator';
 import { RequestUser } from 'src/utils/types/request-user.interface';
 
 import { EmployeeMasterlistService } from './employee-masterlist.service';
+import { ManualSmsNotificationPreferenceDto, SmsNotificationPreferenceDto } from './dto/sms.dto';
+import { OtpVerificationDto } from './dto/otp.dto';
 
 // @ApiCookieAuth('access-token')
 @ApiTags('Human Resources - Employees (Employee Masterlist)')
@@ -86,6 +88,26 @@ export class EmployeeMasterlistController {
     @SessionUser() user: RequestUser,
   ) {
     return this.employeeMasterlistService.createEmployee(createDto, user);
+  }
+
+  @Post('employees/sms-notification-registration')
+  @ApiBody({
+    type: SmsNotificationPreferenceDto,
+    description: 'Payload to register employee to sms notifications'
+  })
+  @ApiOperation({ summary: 'Employee SMS Registration' })
+  smsNotificationRegistration(
+    @Body() dto: ManualSmsNotificationPreferenceDto,
+  ) {
+    return this.employeeMasterlistService.employeeManualSmsNotificationRegistration(dto)
+  }
+
+  @Post('employees/sms-notification-registration/verify-otp')
+  @ApiOperation({ summary: 'Verify otp of employee' })
+  verifyEmployeeSmsOtp(
+    @Body() dto: OtpVerificationDto,
+  ) {
+    return this.employeeMasterlistService.employeeSmsVerify(dto);
   }
 
   //can edit employee profile
