57c1c1c18d25e8a9c1965307873827c591a42d8f
erzan12 <do.earljan@gmail.com>
2026-08-04 13:16:41 +0800

    feat: added otp verification helper for getting otp expiration date
    and generating otp for 6 random digits

--- src/utils/constants/otp-verification.constants.ts
+++ src/utils/constants/otp-verification.constants.ts
@@ -2,8 +2,24 @@ import { randomInt } from 'crypto';
 
 export const OTP_VERIFICATION = {
   FORGOT_PASSWORD: 'ForgotPassword',
-};
+} as const;
 
-export function generateOtp(): string {
-  return randomInt(100000, 999999).toString();
+export const OTP_EXPIRATION = {
+  ONE_MINUTE: 1,
+  THREE_MINUTES: 3,
+  FIVE_MINUTES: 5,
+  TEN_MINUTES: 10,
+} as const;
+
+export function generateOtp(length = 6): string {
+  const min = 10 ** (length - 1);
+  const max = 10 ** length;
+
+  return randomInt(min, max).toString();
+}
+
+export function getOtpExpiration(
+  minutes: (typeof OTP_EXPIRATION)[keyof typeof OTP_EXPIRATION] = OTP_EXPIRATION.FIVE_MINUTES,
+): Date {
+  return new Date(Date.now() + minutes * 60 * 1000);
 }
