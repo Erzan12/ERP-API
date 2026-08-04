48ef3e9431fcb5fd7c7d5d06e19869f0cb4028de
erzan12 <do.earljan@gmail.com>
2026-08-04 13:33:37 +0800

    feat: new migration for sms notification and otp verification

---
+++ prisma/migrations/20260803025319_added_enum_type_attribute_in_otp_schema/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Changed the type of `purpose` on the `OtpVerification` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "OtpVerification" DROP COLUMN "purpose",
+ADD COLUMN     "purpose" "OtpPurposeTemplate" NOT NULL;
