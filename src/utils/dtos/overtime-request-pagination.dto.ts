675782465b541a2f3e84a519cc293b928f02bf80
erzan12 <do.earljan@gmail.com>
2026-08-07 10:55:28 +0800

    feat: added new optional fitler query for ot request list
    date filed from and date filed to pagination filter

--- src/utils/dtos/overtime-request-pagination.dto.ts
+++ src/utils/dtos/overtime-request-pagination.dto.ts
@@ -19,6 +19,38 @@ export class OvertimeRequestsPaginationDto {
   // @ApiPropertyOptional({ default: '' })
   // is_active?: boolean;
 
+  // @IsOptional()
+  // @IsString()
+  // @ApiPropertyOptional({
+  //   example: '18:00:00',
+  //   description: 'Filter records with time_from >= this time',
+  // })
+  // time_from?: string;
+
+  // @IsOptional()
+  // @IsString()
+  // @ApiPropertyOptional({
+  //   example: '22:00:00',
+  //   description: 'Filter records with time_to <= this time',
+  // })
+  // time_to: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    example: '2026-07-01',
+    description: 'Filter record from date filed',
+  })
+  date_filed_from?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    example: '2026-07-20',
+    description: 'Filter record up to',
+  })
+  date_filed_to?: string;
+
   @IsOptional()
   @IsString()
   @ApiPropertyOptional({ example: 'created_at', default: 'created_at' })
