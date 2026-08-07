248325f4e71ac71b0db128a2e776597668141e4a
erzan12 <do.earljan@gmail.com>
2026-08-07 09:58:18 +0800

    feat: added new api in sms service
    employee manual sms notif reg and verify otp

--- src/jobs/sms/sms.module.ts
+++ src/jobs/sms/sms.module.ts
@@ -1,10 +1,13 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { SmsController } from './sms.controller';
+import { PrismaService } from 'src/config/prisma/prisma.service';
 
 @Module({
   imports: [HttpModule],
-  providers: [SmsService],
+  providers: [SmsService, PrismaService],
   exports: [SmsService],
+  controllers: [SmsController],
 })
 export class SmsModule {}
