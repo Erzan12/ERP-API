a7805b8ff2b9c8580d59d953211601362c6b66ba
erzan12 <do.earljan@gmail.com>
2026-08-25 13:40:14 +0800

    feat: added new case close api now this is the last stage of the case
    and will be executed upon clicking close button in the disciplinary case, case close tab

--- src/modules/hris/employee-relations/case-close/case-close.controller.ts
+++ src/modules/hris/employee-relations/case-close/case-close.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, ParseUUIDPipe, Put } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CaseCloseService } from './case-close.service';
+import { SessionUser } from 'src/utils/decorators/session-user.decorator';
+import { RequestUser } from 'src/utils/types/request-user.interface';
 
-@Controller('case-close')
-export class CaseCloseController {}
+@ApiTags('Human Resources - Employee Relations(Case Close)')
+@Controller({ path: 'hris', version: '2' })
+export class CaseCloseController {
+    constructor (private readonly caseCloseService: CaseCloseService) {}
+
+    @Put('employee-relations/discplinary-case/:disciplinaryCaseId/case-close')
+    @ApiOperation({
+        summary: 'Close disciplinary case',
+        description: 
+            'Closes the disciplinary case and exits the case_cloased stage for all respondents.',
+    })
+    closeCase(
+        @Param('disciplinaryCaseId', new ParseUUIDPipe()) disciplinaryCaseId: string,
+        @SessionUser() user: RequestUser,
+    ) {
+        return this.caseCloseService.closeCase(disciplinaryCaseId, user);
+    }
+}
