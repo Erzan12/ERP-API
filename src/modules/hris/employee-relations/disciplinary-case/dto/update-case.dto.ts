5ecb0695ef0e8d1ff80bb5752fb00256a2066a85
erzan12 <do.earljan@gmail.com>
2026-08-27 14:39:35 +0800

    patch: fix linting errors

--- src/modules/hris/employee-relations/disciplinary-case/dto/update-case.dto.ts
+++ src/modules/hris/employee-relations/disciplinary-case/dto/update-case.dto.ts
@@ -1,5 +1,5 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsDateString, IsOptional, IsString, IsUUID } from "class-validator";
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsDateString, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class UpdateCaseDto {
   @IsUUID()
