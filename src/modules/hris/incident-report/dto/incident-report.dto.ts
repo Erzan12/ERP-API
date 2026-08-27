4ccb3d16ef53f1f74d6890704be1233f0a77b4ea
erzan12 <do.earljan@gmail.com>
2026-08-27 14:04:48 +0800

    feat: added full crud operation to incident report submodule
    with fully customizable parties/employees involved

--- src/modules/hris/incident-report/dto/incident-report.dto.ts
+++ src/modules/hris/incident-report/dto/incident-report.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { ArrayMinSize, ArrayNotEmpty, IsDateString, IsNotEmpty, IsOptional, IsString, IsUUID, ValidateNested } from "class-validator";
 import { SharedCreateCaseIntakePartydto } from "./shared-create-case-intake-party.dto";
@@ -29,4 +29,32 @@ export class CreateIncidentReportDto {
     @ArrayNotEmpty()
     @ApiProperty({ type: [String], description: 'Committed offense IDs' })
     offense_ids: string[];
+}
+
+export class UpdateIncidentReportDto {
+    @IsUUID()
+    @IsOptional()
+    @ApiPropertyOptional({ example: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890' })
+    incident_location_id?: string;
+
+    @IsDateString()
+    @IsOptional()
+    @ApiPropertyOptional({ example: '2026-08-20T09:00:00.000Z' })
+    incident_date?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiPropertyOptional({ required: false, example: 'The personnel jump off the vessel' })
+    incident_narrative?: string;
+
+    @ValidateNested({ each: true })
+    @Type(() => SharedCreateCaseIntakePartydto)
+    @ArrayMinSize(1)
+    @ApiPropertyOptional({ type: [SharedCreateCaseIntakePartydto] })
+    parties?: SharedCreateCaseIntakePartydto[];
+
+    @IsUUID('4', { each: true })
+    // @ArrayO()
+    @ApiPropertyOptional({ type: [String], description: 'Committed offense IDs' })
+    offense_ids?: string[];
 }
