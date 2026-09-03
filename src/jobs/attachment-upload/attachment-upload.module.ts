dfa6889357947124ce48e64e948290f6c9dc907a
erzan12 <do.earljan@gmail.com>
2026-09-03 09:18:29 +0800

    feat: remove attachment upload controller updated module and service

--- src/jobs/attachment-upload/attachment-upload.module.ts
+++ src/jobs/attachment-upload/attachment-upload.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { AttachmentUploadService } from './attachment-upload.service';
+import { PrismaService } from 'src/config/prisma/prisma.service';
+import { ErCaseAttachmentController } from './module/er-case-attachment/er-case-attachment.controller';
+import { ErCaseAttachmentService } from './module/er-case-attachment/er-case-attachment.service';
 
-@Module({})
+@Module({
+    imports: [AuthModule],
+    providers: [
+        PrismaService,
+        AttachmentUploadService,
+        ErCaseAttachmentService,
+    ],
+    controllers: [
+        ErCaseAttachmentController,
+    ]
+})
 export class AttachmentUploadModule {}
