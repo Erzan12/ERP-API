2ab45715c7e1f531832b3fccdf710d5207470f62
erzan12 <do.earljan@gmail.com>
2026-09-15 14:38:56 +0800

    feat: added new api for uploading written explaination docs
    to avega minio cloud storage

--- src/utils/constants/transaction-type.constants.ts
+++ src/utils/constants/transaction-type.constants.ts
@@ -4,4 +4,5 @@ export const TRANSACTION_TYPE = {
   ER_CASE_DOC: 'ER Case Document',
   IR_ER_DOC: 'IR or ER Document',
   NTE_DOC: 'NTE Document',
+  WRITTEN_EXPLANATION_DOC: 'Written Explanation Document',
 };
