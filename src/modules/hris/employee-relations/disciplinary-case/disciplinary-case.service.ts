a5c586cd1e59028753f44845a6b227191ad1afbf
erzan12 <do.earljan@gmail.com>
2026-09-17 14:11:39 +0800

    fix: run lint check to fix linting errors

--- src/modules/hris/employee-relations/disciplinary-case/disciplinary-case.service.ts
+++ src/modules/hris/employee-relations/disciplinary-case/disciplinary-case.service.ts
@@ -524,7 +524,7 @@ export class DisciplinaryCaseService {
                     },
                   },
                 },
-                attachment: true
+                attachment: true,
               },
             },
             decision: true,
